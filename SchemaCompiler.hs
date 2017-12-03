7a50a41caafa4d03c0f752b3cddda25c33d17159
Marc Scholten <marc@digitallyinduced.com>
2017-12-03 21:48:00 +0100

    Added create, find, findAll functions to SchemaCompiler

--- SchemaCompiler.hs
+++ SchemaCompiler.hs
@@ -10,18 +10,39 @@ import qualified Data.Text as Text
 -- USE LINE PRAGMA IN OUTPUT
 {-# LINE 42 "Foo.vhs" #-}
 
+c = compile
 compile :: IO ()
-compile = putStrLn $ intercalate "\n\n" $ map compileTable database
+compile = do
+    let compiled = map (\table -> (getFilePath table, compileTable table)) database
+    mapM_ writeTable compiled
+
+writeTable :: (FilePath, Text) -> IO ()
+writeTable (path, content) = do
+    writeFile (cs path) (cs content)
 
 section = "\n"
 compileTable table@(Table name attributes) =
-    "module Model." <> tableNameToModelName name <> " where \n"
+    "module Model.Generated." <> tableNameToModelName name <> " where\n\n"
+    <> "import Foundation.HaskellSupport\n"
+    <> "import Foundation.ModelSupport\n"
+    <> "import ClassyPrelude\n"
     <> "import Database.PostgreSQL.Simple\n"
     <> "import Database.PostgreSQL.Simple.FromRow\n"
     <> section
     <> compileDataDefinition table
     <> section
     <> compileFromRowInstance table
+    <> section
+    <> section
+    <> compileCreate table
+    <> section
+    <> compileFindAll table
+    <> section
+    <> compileFind table
+    <> section
+
+getFilePath :: Table -> FilePath
+getFilePath (Table name attributes) = "src/Model/Generated/" <> (cs $ tableNameToModelName name) <> ".hs"
 
 compileDataDefinition :: Table -> Text
 compileDataDefinition table@(Table name attributes) =
@@ -38,7 +59,50 @@ compileDataDefinition table@(Table name attributes) =
 		
 
 compileCreate table@(Table name attributes) =
-    "create = "
+    let
+        modelName = tableNameToModelName name
+        columns = intercalate ", " $ map toColumn attributes
+        toColumn (Field fieldName fieldType) = fieldName
+        values = intercalate ", " $ map toValue attributes
+        toValue (Field fieldName fieldType) =
+            case fieldType of
+                SerialField -> "DEFAULT"
+                otherwise   -> "?"
+        bindings = intercalate ", " $ map toBinding attributes
+        toBinding (Field fieldName fieldType) = fieldName <> " model"
+    in
+        "create :: (?modelContext :: ModelContext) => " <> modelName <> " -> IO " <> modelName <> " \n"
+        <> "create model = do\n"
+        <> indent ("let (ModelContext conn) = ?modelContext\n"
+            <> "PG.execute conn \"INSERT INTO " <> name <> " (" <> columns <> ") VALUES (" <> values <> ")\" [" <> bindings <> "]\n"
+            <> "return model\n"
+        )
+
+compileFindAll table@(Table name attributes) =
+    let
+        modelName = tableNameToModelName name
+    in
+        "findAll :: (?modelContext :: ModelContext) => IO [" <> modelName <> "]\n"
+        <> "findAll = do\n"
+        <> indent (
+            "let (ModelContext conn) = ?modelContext\n"
+            <> "projects <- PG.query_ conn \"SELECT * FROM projects\"\n"
+            <> "return projects\n"
+        )
+
+
+compileFind table@(Table name attributes) =
+    let
+        modelName = tableNameToModelName name
+    in
+        "find :: (?modelContext :: ModelContext) => Int -> IO (Maybe " <> modelName <> ")\n"
+        <> "find id = do\n"
+        <> indent (
+            "let (ModelContext conn) = ?modelContext\n"
+            <> "results <- PG.query conn \"SELECT * FROM " <> name <> " WHERE id = ?\" [id]\n"
+            <> "return $ headMay results\n"
+        )
+
 
 compileFromRowInstance table@(Table name attributes) =
     "instance FromRow " <> tableNameToModelName name <> " where \n"
