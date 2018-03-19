a743dee4185b4d35d6b5b91fce41ff7346107fbe
Marc Scholten <marc@digitallyinduced.com>
2018-03-19 13:47:38 +0100

    Improvements to dev server

--- Commands/RunDevServer.hs
+++ Commands/RunDevServer.hs
@@ -1,134 +1,135 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
 module Main where
-    import ClassyPrelude hiding (threadDelay)
-    import qualified System.Process as Process
-    import Twitch
-    import System.Exit
-    import System.Posix.Signals
-    import qualified Control.Exception as Exception
-    import qualified GHC.IO.Handle as Handle
-    import System.Process.Internals
-    import Data.String.Conversions (cs)
-    import Control.Concurrent (threadDelay)
+import ClassyPrelude hiding (threadDelay)
+import qualified System.Process as Process
+import Twitch
+import qualified Twitch
+import System.Exit
+import System.Posix.Signals
+import qualified Control.Exception as Exception
+import qualified GHC.IO.Handle as Handle
+import System.Process.Internals
+import Data.String.Conversions (cs)
+import Control.Concurrent (threadDelay)
+import qualified System.FSNotify as FS
+import System.Directory (getCurrentDirectory, withCurrentDirectory)
+import Data.Default ()
 
-    runServerHs = "src/Foundation/Commands/RunServer.hs"
+runServerHs = "src/Foundation/Commands/RunServer.hs"
 
-    data DevServerState = DevServerState {
-            postgresProcess :: IORef (Handle, Process.ProcessHandle),
-            serverProcess :: IORef (Handle, Process.ProcessHandle),
-            modelCompilerProcess :: IORef (Handle, Process.ProcessHandle),
-            urlGeneratorCompilerProcess :: IORef (Handle, Process.ProcessHandle)
-        }
+data DevServerState = DevServerState {
+        postgresProcess :: IORef (Handle, Process.ProcessHandle),
+        serverProcess :: IORef (Handle, Process.ProcessHandle),
+        modelCompilerProcess :: IORef (Handle, Process.ProcessHandle),
+        urlGeneratorCompilerProcess :: IORef (Handle, Process.ProcessHandle)
+    }
 
-    initDevServerState = do
-        postgresProcess <- startPostgres >>= newIORef
-        serverProcess <- startPlainGhci >>= initServer >>= newIORef
-        modelCompilerProcess <- startCompileGhci >>= newIORef
-        urlGeneratorCompilerProcess <- startCompileGhci >>= newIORef
-        return $ DevServerState { postgresProcess = postgresProcess, serverProcess = serverProcess, modelCompilerProcess = modelCompilerProcess, urlGeneratorCompilerProcess = urlGeneratorCompilerProcess }
+initDevServerState = do
+    postgresProcess <- startPostgres >>= newIORef
+    serverProcess <- startPlainGhci >>= initServer >>= newIORef
+    modelCompilerProcess <- startCompileGhci >>= newIORef
+    urlGeneratorCompilerProcess <- startCompileGhci >>= newIORef
+    return $ DevServerState { postgresProcess = postgresProcess, serverProcess = serverProcess, modelCompilerProcess = modelCompilerProcess, urlGeneratorCompilerProcess = urlGeneratorCompilerProcess }
 
-    registerExitHandler handler = do
-        threadId <- myThreadId
-        installHandler keyboardSignal (Catch (do { handler; Exception.throwTo threadId ExitSuccess })) Nothing
-
-
-    main = do
-        state <- initDevServerState
-        registerExitHandler (cleanup state)
-        watch state
-        cleanup state
-
-    cleanup :: DevServerState -> IO ()
-    cleanup state = do
-        putStrLn "cleanup"
-        let DevServerState { serverProcess, postgresProcess, modelCompilerProcess, urlGeneratorCompilerProcess } = state
-        let processes = [serverProcess, postgresProcess, modelCompilerProcess, urlGeneratorCompilerProcess]
-        let stopProcess process = do (_, p') <- readIORef serverProcess; Process.terminateProcess p'
-        stopServer
-        forM_ processes stopProcess
-
-    startPlainGhci = do
-        let process = (Process.proc "ghci" ["-threaded", "-isrc", "-fprint-potential-instances"]) { Process.std_in = Process.CreatePipe }
-        (Just input, _, _, handle) <- Process.createProcess process
-        return (input, handle)
-
-    startCompileGhci = do
-        let process = (Process.proc "ghci" ["-threaded", "-isrc", "-w"]) { Process.std_in = Process.CreatePipe }
-        (Just input, _, _, handle) <- Process.createProcess process
-        return (input, handle)
-
-    initServer ghci = do
-        sendGhciCommand ghci (":script src/Foundation/startDevServerGhciScript")
-        return ghci
-
-    watch state@(DevServerState {serverProcess}) = defaultMain $ do
-        "Controller/*.hs" |> const (rebuild serverProcess)
-        "View/*/*.hs" |> const (rebuild serverProcess)
-        "View/Context.hs" |> const (rebuild serverProcess)
-        "Model/Schema.hs" |> const (rebuildModels state)
-        "Model/*.hs" |> const (rebuild serverProcess)
-        "Model/Generated/*.hs" |> const (rebuild serverProcess)
-        "Config.hs" |> const (rebuild serverProcess)
-        "Foundation/**.hs" |> const (rebuild serverProcess)
-        "Routes.hs" |> const (rebuildUrlGenerator state)
-        "UrlGenerator.hs" |> const (rebuild serverProcess)
+registerExitHandler handler = do
+    threadId <- myThreadId
+    installHandler keyboardSignal (Catch (do { handler; Exception.throwTo threadId ExitSuccess })) Nothing
 
 
-    rebuildModels (DevServerState {modelCompilerProcess}) = do
-        putStrLn "rebuildModels"
-        ghci@(input, process) <- readIORef modelCompilerProcess
-        sendGhciCommand ghci ":!clear"
-        sendGhciCommand ghci ":l src/Foundation/SchemaCompiler.hs"
-        sendGhciCommand ghci "c"
-        putStrLn "rebuildModels => Finished"
+main = do
+    state <- initDevServerState
+    registerExitHandler (cleanup state)
+    manager <- withCurrentDirectory "./src" $ Twitch.run (watch state)
+    forever (threadDelay maxBound) `finally` (do FS.stopManager manager; cleanup state)
 
-    rebuildUrlGenerator (DevServerState {urlGeneratorCompilerProcess}) = do
-        putStrLn "rebuildUrlGenerator"
-        ghci@(input, process) <- readIORef urlGeneratorCompilerProcess
-        sendGhciCommand ghci ":l src/Foundation/UrlGeneratorCompiler.hs"
-        sendGhciCommand ghci "c"
-        putStrLn "rebuildUrlGenerator => Finished"
+cleanup :: DevServerState -> IO ()
+cleanup state = do
+    putStrLn "cleanup"
+    let DevServerState { serverProcess, postgresProcess, modelCompilerProcess, urlGeneratorCompilerProcess } = state
+    let processes = [serverProcess, postgresProcess, modelCompilerProcess, urlGeneratorCompilerProcess]
+    let stopProcess process = do (_, p') <- readIORef serverProcess; Process.terminateProcess p'
+    stopServer
+    forM_ processes stopProcess
 
-    sendGhciInterrupt ghci@(input, process) = do
-        pid <- getPid process
-        case pid of
-            Just pid -> signalProcess sigINT pid
-            Nothing -> putStrLn "sendGhciInterrupt: failed, pid not found"
+startPlainGhci = do
+    let process = (Process.proc "ghci" ["-threaded", "-isrc", "-fprint-potential-instances", "-fexternal-interpreter", "-fomit-interface-pragmas"]) { Process.std_in = Process.CreatePipe }
+    (Just input, _, _, handle) <- Process.createProcess process
+    return (input, handle)
 
-    rebuild serverProcess = do
-        putStrLn "Rebuilding server"
-        ghci <- readIORef serverProcess
-        sendGhciInterrupt ghci
-        sendGhciInterrupt ghci
-        sendGhciCommand ghci ""
-        sendGhciCommand ghci ":r"
-        sendGhciCommand ghci "run"
-        putStrLn "Restarted server"
+startCompileGhci = do
+    let process = (Process.proc "ghci" ["-threaded", "-isrc", "-w"]) { Process.std_in = Process.CreatePipe }
+    (Just input, _, _, handle) <- Process.createProcess process
+    return (input, handle)
 
-    sendGhciCommand ghciProcess command = do
-        let (input, process) = ghciProcess
-        putStrLn $ "Sending to ghci: " <> cs command
-        Handle.hPutStr input (command <> "\n")
-        Handle.hFlush input
+initServer ghci = do
+    sendGhciCommand ghci (":script src/Foundation/startDevServerGhciScript")
+    return ghci
 
-    stopServer = do
-        putStrLn "stopServer called"
-        _ <- Process.system "(lsof -i tcp:8000 | grep ghc | awk 'NR!=1 {print $2}' | xargs kill) || true"
-        return ()
+watch state@(DevServerState {serverProcess}) = do
+    "Model/Schema.hs" |> const (rebuildModels state)
+    "Routes.hs" |> const (rebuildUrlGenerator state)
+    "View/*/*.hs" |> const (rebuild serverProcess)
+    "Model/Generated/*.hs" |> const (rebuild serverProcess)
+    "UrlGenerator.hs" |> const (rebuild serverProcess)
+    "**.hs" |> const (rebuild serverProcess)
+    "*/*.hs" |> const (rebuild serverProcess)
+    "*/*/*.hs" |> const (rebuild serverProcess)
+    "*/*/*/*.hs" |> const (rebuild serverProcess)
 
-    stopPostgres = do
-        _ <- Process.system "(lsof -i tcp:8001 | grep postgres | awk 'NR!=1 {print $2}' | xargs kill) || true"
-        return ()
 
-    startPostgres = do
-        let process = (Process.proc "postgres" ["-D", "db/state", "-p", "8001"]) { Process.std_in = Process.CreatePipe }
-        (Just input, _, _, handle) <- Process.createProcess process
+rebuildModels (DevServerState {modelCompilerProcess}) = do
+    putStrLn "rebuildModels"
+    ghci@(input, process) <- readIORef modelCompilerProcess
+    sendGhciCommand ghci ":!clear"
+    sendGhciCommand ghci ":l src/Foundation/SchemaCompiler.hs"
+    sendGhciCommand ghci "c"
+    putStrLn "rebuildModels => Finished"
 
-        return (input, handle) 
-        
-    getPid ph = withProcessHandle ph go
-        where
-            go ph_ = case ph_ of
-                OpenHandle x   -> return $ Just x
-                ClosedHandle _ -> return Nothing
+rebuildUrlGenerator (DevServerState {urlGeneratorCompilerProcess}) = do
+    putStrLn "rebuildUrlGenerator"
+    ghci@(input, process) <- readIORef urlGeneratorCompilerProcess
+    sendGhciCommand ghci ":l src/Foundation/UrlGeneratorCompiler.hs"
+    sendGhciCommand ghci "c"
+    putStrLn "rebuildUrlGenerator => Finished"
+
+sendGhciInterrupt ghci@(input, process) = do
+    pid <- getPid process
+    case pid of
+        Just pid -> signalProcess sigINT pid
+        Nothing -> putStrLn "sendGhciInterrupt: failed, pid not found"
+
+rebuild serverProcess = do
+    putStrLn "Rebuilding server"
+    ghci <- readIORef serverProcess
+    _ <- Process.system "lsof -i :8000|grep ghc-iserv | awk '{print $2}'|head -n1|xargs kill -SIGINT"
+    threadDelay 1000000
+    sendGhciCommand ghci ":script src/Foundation/startDevServerGhciScriptRec"
+    putStrLn "Restarted server"
+
+sendGhciCommand ghciProcess command = do
+    let (input, process) = ghciProcess
+    putStrLn $ "Sending to ghci: " <> cs command
+    Handle.hPutStr input (command <> "\n")
+    Handle.hFlush input
+
+stopServer = do
+    putStrLn "stopServer called"
+    _ <- Process.system "(lsof -i :8000|grep ghc-iserv | awk '{print $2}'|head -n1|xargs kill -9) || true"
+    return ()
+
+stopPostgres = do
+    _ <- Process.system "(lsof -i tcp:8001 | grep postgres | awk 'NR!=1 {print $2}' | xargs kill) || true"
+    return ()
+
+startPostgres = do
+    let process = (Process.proc "postgres" ["-D", "db/state", "-p", "8001"]) { Process.std_in = Process.CreatePipe }
+    (Just input, _, _, handle) <- Process.createProcess process
+
+    return (input, handle)
+
+getPid ph = withProcessHandle ph go
+    where
+        go ph_ = case ph_ of
+            OpenHandle x   -> return $ Just x
+            ClosedHandle _ -> return Nothing
--- Server.hs
+++ Server.hs
@@ -1,45 +1,48 @@
 module Foundation.Server (run) where
-    import ClassyPrelude
-    import qualified Network.Wai.Handler.Warp as Warp
-    import Network.Wai
-    import Foundation.Router (Router, match)
+import ClassyPrelude
+import qualified Network.Wai.Handler.Warp as Warp
+import Network.Wai
+import Foundation.Router (Router, match)
 
-    import Network.Wai.Middleware.RequestLogger (logStdoutDev)
-    import Network.Wai.Middleware.MethodOverridePost (methodOverridePost)
-    import Network.Wai.Middleware.Static
-    import Network.HTTP.Types.Status (status404)
-    import Network.Wai.Session (withSession, Session)
-    import Network.Wai.Session.ClientSession (clientsessionStore)
-    import Web.ClientSession (getDefaultKey)
-    import qualified Data.Vault.Lazy as Vault
-    import Data.Default (def)
-    import Network.Wai.Session.Map (mapStore_)
-    import qualified Web.Cookie
-    import qualified Data.Time.Clock
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import Network.Wai.Middleware.MethodOverridePost (methodOverridePost)
+import Network.Wai.Middleware.Static
+import Network.HTTP.Types.Status (status404)
+import Network.Wai.Session (withSession, Session)
+import Network.Wai.Session.ClientSession (clientsessionStore)
+import Web.ClientSession (getDefaultKey)
+import qualified Data.Vault.Lazy as Vault
+import Data.Default (def)
+import Network.Wai.Session.Map (mapStore_)
+import qualified Web.Cookie
+import qualified Data.Time.Clock
 
-    import Foundation.ModelSupport
-    import Foundation.ApplicationContext
+import Foundation.ModelSupport
+import Foundation.ApplicationContext
 
-    import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple
 
-    import qualified Routes
-    import qualified Config
-    import qualified Foundation.LoginSupport.Middleware
+import qualified Routes
+import qualified Config
+import qualified Foundation.LoginSupport.Middleware
 
-    defaultPort :: Int
-    defaultPort = 8000
+defaultPort :: Int
+defaultPort = 8000
 
-    run :: IO ()
-    run = do
-        conn <- connectPostgreSQL Config.postgreSQLUrl
-        session <- Vault.newKey
-        store <- fmap clientsessionStore getDefaultKey
-        let applicationContext = ApplicationContext (ModelContext conn) session
-        Warp.runEnv defaultPort $ withSession store "SESSION" (def { Web.Cookie.setCookiePath = Just "/", Web.Cookie.setCookieMaxAge = Just (Data.Time.Clock.secondsToDiffTime 60 * 60 * 24 * 30) }) session $ logStdoutDev $ staticPolicy (addBase "static/") $ Foundation.LoginSupport.Middleware.middleware applicationContext $ methodOverridePost $ application Routes.match applicationContext
 
-    -- TODO: logger middleware
-    application :: Router -> ApplicationContext -> Application
-    application router applicationContext request respond = do
-        case match request router of
-            Just application -> application applicationContext request respond
-            Nothing -> respond $ responseLBS status404 [] "Not found!"
+
+run :: IO ()
+run = do
+    putStrLn "Start"
+    conn <- connectPostgreSQL Config.postgreSQLUrl
+    session <- Vault.newKey
+    store <- fmap clientsessionStore getDefaultKey
+    let applicationContext = ApplicationContext (ModelContext conn) session
+    Warp.runEnv defaultPort $ withSession store "SESSION" (def { Web.Cookie.setCookiePath = Just "/", Web.Cookie.setCookieMaxAge = Just (Data.Time.Clock.secondsToDiffTime 60 * 60 * 24 * 30) }) session $ logStdoutDev $ staticPolicy (addBase "static/") $ Foundation.LoginSupport.Middleware.middleware applicationContext $ methodOverridePost $ application Routes.match applicationContext
+
+-- TODO: logger middleware
+application :: Router -> ApplicationContext -> Application
+application router applicationContext request respond = do
+    case match request router of
+        Just application -> application applicationContext request respond
+        Nothing -> respond $ responseLBS status404 [] "Not found!"
