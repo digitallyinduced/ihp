f79391dd733bad8fde3e62ce480765a225cb9a69
Marc Scholten <marc@digitallyinduced.com>
2018-03-26 17:53:46 +0200

    Added ToHtml ByteString instance

--- HtmlSupport/ToHtml.hs
+++ HtmlSupport/ToHtml.hs
@@ -6,6 +6,7 @@ import qualified Text.Blaze.Internal
 import ClassyPrelude (Text, Show, show)
 import Data.String.Conversions (cs)
 import Foundation.View.ConvertibleStrings ()
+import qualified Data.ByteString
 
 class ToHtml a where
     toHtml :: a -> Html5.Html
@@ -15,6 +16,9 @@ instance ToHtml (Text.Blaze.Internal.MarkupM ()) where
 
 instance ToHtml Text where
     toHtml = cs
+
+instance ToHtml Data.ByteString.ByteString where
+    toHtml value = toHtml (cs value :: Text)
 
 instance Show a => ToHtml a where
     toHtml value = cs (show value)
