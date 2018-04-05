2e2b11477d485b651c43df1ef4d6cea22b33672f
Marc Scholten <marc@digitallyinduced.com>
2018-04-05 19:32:04 +0200

    Lots of refactoring and updates

---
+++ AuthSupport/Authentication.hs
@@ -0,0 +1,22 @@
+module Foundation.AuthSupport.Authentication (PasswordLogin (getPasswordHash), verifyPassword, hashPassword, generateAuthenticationToken) where
+
+import ClassyPrelude
+import Data.String.Conversions (cs)
+import qualified Crypto.PasswordStore
+import Data.String.Conversions (cs)
+import qualified Test.RandomStrings
+
+class PasswordLogin entity where
+    getPasswordHash :: entity -> Text
+
+passwordStrength :: Int
+passwordStrength = 17
+
+hashPassword :: ByteString -> IO Text
+hashPassword plainText = (Crypto.PasswordStore.makePassword plainText passwordStrength) >>= return . cs
+
+verifyPassword :: PasswordLogin entity => entity -> Text -> Bool
+verifyPassword entity plainText = Crypto.PasswordStore.verifyPassword (cs plainText) (cs (getPasswordHash entity))
+
+generateAuthenticationToken :: IO Text
+generateAuthenticationToken = (Test.RandomStrings.randomWord Test.RandomStrings.randomASCII 32) >>= return . cs
