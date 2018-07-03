a71edfbf40fb829b9626cc1e8b9f266b713a5d27
Marc Scholten <marc@digitallyinduced.com>
2018-07-03 11:12:55 +0200

    Preparation for queryBuilder include

--- ModelSupport.hs
+++ ModelSupport.hs
@@ -158,9 +158,12 @@ findMany ids = do
     let tableName = symbolVal @(GetTableName (GetModelById id)) Proxy
     sqlQuery (PG.Query $ "SELECT * FROM " <> cs tableName <> " WHERE id IN ?") (PG.Only $ PG.In ids)
 
+
 class ColumnNames model where
     type ColumnNamesRecord model :: GHC.Types.Type
     columnNames :: Proxy model -> ColumnNamesRecord model
 
 type family ModelFieldType model :: GHC.Types.Type
 type family ModelFieldValue model (field :: GHC.Types.Symbol) :: GHC.Types.Type
+
+type family Include (name :: GHC.Types.Symbol) model
