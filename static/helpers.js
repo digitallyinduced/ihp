62ced21da38e22c43338f050f84ffe7b496ac122
Marc Scholten <marc@digitallyinduced.com>
2018-08-14 16:11:06 +0200

    Fixed build

--- static/helpers.js
+++ static/helpers.js
@@ -227,3 +227,74 @@ function initToggle() {
 function initDatePicker() {
     flatpickr("input[type='date']", {});
 }
+
+var locked = false;
+var debugMorphdom = false;
+window.transitionToNewPage = function (newBody) {
+    if (locked) {
+        return;
+    }
+
+    console.log(1, document.body)
+    var isModalOpen = document.body.classList.contains('modal-open');
+    morphdom(document.body, newBody, {
+        childrenOnly: true,
+        onBeforeElUpdated: function (from, to) {
+
+            if (newBody.classList.contains('modal-open') && from.id === 'main-row') {
+                return false;
+            } else if (isModalOpen && from.id === 'main-row') {
+                return false;
+            } else if (from.parentNode && from.parentNode.tagName === 'svg') {
+                return false;
+            }
+
+            if (debugMorphdom)
+                console.log('onBeforeElUpdated', from, to)
+        },
+        onBeforeElChildrenUpdated: function (from, to) {
+            if (debugMorphdom)
+                console.log('onBeforeElChildrenUpdated', from, to);
+        },
+        onBeforeNodeDiscarded: function (el) {
+            if (el && el.classList && el.classList.contains && el.classList.contains('animate-delete')) {
+                el.style.height = window.getComputedStyle(el).height;
+                // el.style.height = window.getComputedStyle(el).height;
+
+                setTimeout(function (el) {
+                    el.classList.add('delete');
+                }, 0, el);
+                setTimeout(function (el) {
+                    el.parentNode.removeChild(el);
+                }, 300, el);
+                return false;
+            }
+
+            if (debugMorphdom)
+                console.log('onBeforeNodeDiscarded', el);
+        },
+        getNodeKey: function (el) {
+            var key = el.id;
+            if (el.id) {
+                key = el.id;
+            } else if (el.form && el.name) {
+                key = el.name + "_" + el.form.action;
+            } else if (el instanceof HTMLFormElement) {
+                key = "form#" + el.action;
+            } else if (el instanceof HTMLScriptElement) {
+                key = el.src;
+            }
+            return key;
+        },
+        onBeforeNodeAdded: function (el) {
+            if (debugMorphdom)
+                console.log('onBeforeNodeAdded', el);
+        }
+    });
+
+    locked = true;
+
+    setTimeout(function () {
+        locked = false;
+    }, 100);
+}
--- static/vendor/turbolinksMorphdom.js
+++ static/vendor/turbolinksMorphdom.js
@@ -1,5 +1,3 @@
-var locked = false;
-
 if (window.Turbolinks) {
     var oldAssignNewBody = Turbolinks.SnapshotRenderer.prototype.assignNewBody;
 
@@ -12,73 +10,4 @@ if (window.Turbolinks) {
         this.scrollToTop = function () { };
         oldPerformScroll.call(this);
     };
-}
-
-var debugMorphdom = false;
-window.transitionToNewPage = function (newBody) {
-    if (locked) {
-        return;
-    }
-
-    var isModalOpen = document.body.classList.contains('modal-open');
-    morphdom(document.body, newBody, {
-        childrenOnly: true,
-        onBeforeElUpdated: function (from, to) {
-
-            if (newBody.classList.contains('modal-open') && from.id === 'main-row') {
-                return false;
-            } else if (isModalOpen && from.id === 'main-row') {
-                return false;
-            } else if (from.parentNode && from.parentNode.tagName === 'svg') {
-                return false;
-            }
-
-            if (debugMorphdom)
-                console.log('onBeforeElUpdated', from, to)
-        },
-        onBeforeElChildrenUpdated: function (from, to) {
-            if (debugMorphdom)
-                console.log('onBeforeElChildrenUpdated', from, to);
-        },
-        onBeforeNodeDiscarded: function (el) {
-            if (el && el.classList && el.classList.contains && el.classList.contains('animate-delete')) {
-                el.style.height = window.getComputedStyle(el).height;
-                // el.style.height = window.getComputedStyle(el).height;
-
-                setTimeout(function (el) {
-                    el.classList.add('delete');
-                }, 0, el);
-                setTimeout(function (el) {
-                    el.parentNode.removeChild(el);
-                }, 300, el);
-                return false;
-            }
-
-            if (debugMorphdom)
-                console.log('onBeforeNodeDiscarded', el);
-        },
-        getNodeKey: function (el) {
-            var key = el.id;
-            if (el.id) {
-                key = el.id;
-            } else if (el.form && el.name) {
-                key = el.name + "_" + el.form.action;
-            } else if (el instanceof HTMLFormElement) {
-                key = "form#" + el.action;
-            } else if (el instanceof HTMLScriptElement) {
-                key = el.src;
-            }
-            return key;
-        },
-        onBeforeNodeAdded: function (el) {
-            if (debugMorphdom)
-                console.log('onBeforeNodeAdded', el);
-        }
-    });
-
-    locked = true;
-
-    setTimeout(function () {
-        locked = false;
-    }, 100);
 }
