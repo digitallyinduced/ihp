129f7fdab3e305ab7490ecce6eb2382bae1f7d0e
Marc Scholten <marc@digitallyinduced.com>
2018-08-22 10:27:02 +0200

    Allow for custom field validations

--- ValidationSupport.hs
+++ ValidationSupport.hs
@@ -6,7 +6,8 @@ module Foundation.ValidationSupport (
         module Foundation.ValidationSupport.Types,
         module Foundation.ValidationSupport.ValidateCanView,
         module Foundation.ValidationSupport.ValidateField,
-        module Foundation.ValidationSupport.ValidateIsUnique
+        module Foundation.ValidationSupport.ValidateIsUnique,
+        module Foundation.ValidationSupport.ValidateFieldIO
     )
 where
 
@@ -14,3 +15,4 @@ import Foundation.ValidationSupport.Types
 import Foundation.ValidationSupport.ValidateCanView
 import Foundation.ValidationSupport.ValidateIsUnique
 import Foundation.ValidationSupport.ValidateField
+import Foundation.ValidationSupport.ValidateFieldIO
