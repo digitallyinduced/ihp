99f3e7205b71d90afe06ae64b2eb85e8fe66937f
Marc Scholten <marc@digitallyinduced.com>
2019-06-18 17:22:46 +0200

    Added missing exports

--- TurboHaskell/SchemaPrelude.hs
+++ TurboHaskell/SchemaPrelude.hs
@@ -1,7 +1,9 @@
 module TurboHaskell.SchemaPrelude
 ( module ClassyPrelude
 , module TurboHaskell.SchemaSupport
+, module TurboHaskell.SchemaTypes
 ) where
 
 import ClassyPrelude hiding ((+), point, bool)
 import TurboHaskell.SchemaSupport
+import TurboHaskell.SchemaTypes
