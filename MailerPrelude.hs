a14bd68f182402afcf8cd2f5afd9622907511f12
Marc Scholten <marc@digitallyinduced.com>
2019-07-30 14:39:24 +0200

    Fixed MailerPrelude

--- MailerPrelude.hs
+++ MailerPrelude.hs
@@ -3,19 +3,15 @@ module TurboHaskell.MailerPrelude (
         module Network.Mail.Mime,
         module Generated.Types,
         module TurboHaskell.HaskellSupport,
-        module UrlGenerator,
         cs,
         plain,
-        baseUrl
     ) where
 
 import           ClassyPrelude
-import           Config                    (baseUrl)
 import           Data.String.Conversions   (cs)
 import qualified Data.String.Interpolate
 import           TurboHaskell.HaskellSupport
 import           Generated.Types
 import           Network.Mail.Mime
-import           UrlGenerator
 
 plain = Data.String.Interpolate.i
