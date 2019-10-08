6e7ef7f98a599e39d15725b526d66d9b9bbcffdd
Marc Scholten <marc@digitallyinduced.com>
2019-10-08 09:08:55 +0200

    Disabled auto refresh

--- Commands/RunDevServer.hs
+++ Commands/RunDevServer.hs
@@ -66,7 +66,6 @@ renderErrorView code isCompiling = [hsx|
         <html lang="en">
           <head>
             <meta charset="utf-8"/>
-            <meta http-equiv="refresh" content="0.5"/>
             <title>Compilation Error</title>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
           </head>
