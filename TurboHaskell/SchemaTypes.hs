0def426f49fc214f73bba234858f3b3a5b744876
Marc Scholten <marc@digitallyinduced.com>
2020-03-18 15:09:26 +0100

    Cleanedup SchemaTypes and removed unused code

--- TurboHaskell/SchemaTypes.hs
+++ TurboHaskell/SchemaTypes.hs
@@ -15,12 +15,7 @@ data Table = Table Text [Attribute]
 data Attribute = Field Text FieldType
                | BelongsTo Text
                | HasMany { name :: Text, inverseOf :: Maybe Text }
-               deriving (Eq, Ord)
-
-instance Show Attribute where
-  show (Field fieldName fieldType) = "Field " <> show fieldName <> " (" <> show fieldType <> ")"
-  show (BelongsTo name) = "BelongsTo " <> show name
-  show (HasMany name inverseOf) = "HasMany " <> show name <> " " <> "(" <> show inverseOf <> ")"
+               deriving (Eq, Ord, Show)
 
 newtype SqlType = SqlType Text
 type Name = Text
@@ -34,23 +29,10 @@ data FieldType =
            | TextField   { defaultValue :: Maybe DefaultValue, references :: Maybe Text, allowNull :: Bool, isPrimaryKey :: Bool, unique :: Bool }
            | IntField    { defaultValue :: Maybe DefaultValue, references :: Maybe Text, allowNull :: Bool, isPrimaryKey :: Bool, unique :: Bool }
            | BoolField   { defaultValue :: Maybe DefaultValue, references :: Maybe Text, allowNull :: Bool, isPrimaryKey :: Bool, unique :: Bool }
-           | EnumField   { defaultValue :: Maybe DefaultValue, references :: Maybe Text, values :: [Text], allowNull :: Bool, isPrimaryKey :: Bool, unique :: Bool }
+           | EnumField   { defaultValue :: Maybe DefaultValue, references :: Maybe Text, allowNull :: Bool, isPrimaryKey :: Bool, unique :: Bool, values :: [Text] }
            | UUIDField   { defaultValue :: Maybe DefaultValue, references :: Maybe Text, allowNull :: Bool, isPrimaryKey :: Bool, onDelete :: OnDelete, unique :: Bool }
            | Timestamp   { defaultValue :: Maybe DefaultValue, references :: Maybe Text, allowNull :: Bool, isPrimaryKey :: Bool, unique :: Bool }
            | PointField  { defaultValue :: Maybe DefaultValue, references :: Maybe Text, allowNull :: Bool, isPrimaryKey :: Bool, unique :: Bool }
            | FloatField  { defaultValue :: Maybe DefaultValue, references :: Maybe Text, allowNull :: Bool, isPrimaryKey :: Bool, unique :: Bool }
            | DoubleField  { defaultValue :: Maybe DefaultValue, references :: Maybe Text, allowNull :: Bool, isPrimaryKey :: Bool, unique :: Bool }
-           deriving (Eq, Ord)
-
-instance Show FieldType where
-    show SerialField { defaultValue, references, allowNull, isPrimaryKey, unique } = "SerialField (" <> show defaultValue <> ") (" <> show references <> ") " <> show allowNull <> " " <> show isPrimaryKey <> " " <> show unique
-    show TextField { defaultValue, references, allowNull, isPrimaryKey, unique } = "TextField (" <> show defaultValue <> ") (" <> show references <> ") " <> show allowNull <> " " <> show isPrimaryKey <> " " <> show unique
-    show IntField { defaultValue, references, allowNull, isPrimaryKey, unique } = "IntField (" <> show defaultValue <> ") (" <> show references <> ") " <> show allowNull <> " " <> show isPrimaryKey <> " " <> show unique
-    show BoolField { defaultValue, references, allowNull, isPrimaryKey, unique } = "BoolField (" <> show defaultValue <> ") (" <> show references <> ") " <> show allowNull <> " " <> show isPrimaryKey <> " " <> show unique
-    show UUIDField { defaultValue, references, allowNull, isPrimaryKey, onDelete, unique } = "UUIDField (" <> show defaultValue <> ") (" <> show references <> ") " <> show allowNull <> " " <> show isPrimaryKey <> " " <> show onDelete <> " " <> show unique
-    show Timestamp { defaultValue, references, allowNull, isPrimaryKey, unique } = "Timestamp (" <> show defaultValue <> ") (" <> show references <> ") " <> show allowNull <> " " <> show isPrimaryKey <> " " <> show unique
-    show PointField { defaultValue, references, allowNull, isPrimaryKey, unique } = "PointField (" <> show defaultValue <> ") (" <> show references <> ") " <> show allowNull <> " " <> show isPrimaryKey <> " " <> show unique
-    show EnumField { values, defaultValue, references, allowNull, isPrimaryKey, unique } = "EnumField (" <> show defaultValue <> ") (" <> show references <> ") (" <> show values <> ") " <> show allowNull <> " " <> show isPrimaryKey <> " " <> show unique
-    show FloatField { defaultValue, references, allowNull, isPrimaryKey, unique } = "FloatField (" <> show defaultValue <> ") (" <> show references <> ") " <> show allowNull <> " " <> show isPrimaryKey <> " " <> show unique
-    show DoubleField { defaultValue, references, allowNull, isPrimaryKey, unique } = "DoubleField (" <> show defaultValue <> ") (" <> show references <> ") " <> show allowNull <> " " <> show isPrimaryKey <> " " <> show unique
-
+           deriving (Eq, Ord, Show)
