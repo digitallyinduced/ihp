a7a2861e6d1cc1afaae32a644479f8496a09c3d3
Marc Scholten <marc@digitallyinduced.com>
2020-03-20 14:26:42 +0100

    Fixed login module

--- TurboHaskell/LoginSupport/Helper/Controller.hs
+++ TurboHaskell/LoginSupport/Helper/Controller.hs
@@ -5,54 +5,57 @@ module TurboHaskell.LoginSupport.Helper.Controller (currentUser, currentUserOrNo
 import TurboHaskell.HaskellSupport
 import Data.Proxy (Proxy (Proxy))
 import ClassyPrelude
-import TurboHaskell.LoginSupport.Types (throwNotLoggedIn, HasNewSessionUrl (newSessionUrl))
+import TurboHaskell.LoginSupport.Types
 import TurboHaskell.Controller.RequestContext
 import qualified TurboHaskell.Controller.Session as Session
 import qualified TurboHaskell.ModelSupport as ModelSupport
 import TurboHaskell.ControllerSupport
 import GHC.TypeLits
 import GHC.Records
+import TurboHaskell.ControllerPrelude
+import TurboHaskell.FrameworkConfig
+import System.IO.Unsafe (unsafePerformIO)
 
 type family CurrentUserRecord
 type family CurrentAdminRecord
 
 {-# INLINE currentUser #-}
-currentUser :: forall user. (?controllerContext :: ControllerContext, HasNewSessionUrl user, Typeable user, user ~ CurrentUserRecord) => user
-currentUser = fromMaybe (throwNotLoggedIn (pure (newSessionUrl (Proxy @user)))) currentUserOrNothing
+currentUser :: forall user. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, FrameworkConfig, HasNewSessionUrl user, Typeable user, user ~ CurrentUserRecord) => user
+currentUser = fromMaybe (redirectToLogin (newSessionUrl (Proxy @user))) currentUserOrNothing
 
 {-# INLINE currentUserOrNothing #-}
-currentUserOrNothing :: forall user. (?controllerContext :: ControllerContext, HasNewSessionUrl user, Typeable user, user ~ CurrentUserRecord) => (Maybe user)
+currentUserOrNothing :: forall user. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, FrameworkConfig, HasNewSessionUrl user, Typeable user, user ~ CurrentUserRecord) => (Maybe user)
 currentUserOrNothing = fromControllerContext @(Maybe user)
 
 {-# INLINE currentUserId #-}
-currentUserId :: forall user userId. (?controllerContext :: ControllerContext, HasNewSessionUrl user, HasField "id" user userId, Typeable user, user ~ CurrentUserRecord) => userId
+currentUserId :: forall user userId. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, FrameworkConfig, HasNewSessionUrl user, HasField "id" user userId, Typeable user, user ~ CurrentUserRecord) => userId
 currentUserId = currentUser @user |> get #id
 
 {-# INLINE ensureIsUser #-}
-ensureIsUser :: forall user userId. (?controllerContext :: ControllerContext, HasNewSessionUrl user, HasField "id" user userId, Typeable user, user ~ CurrentUserRecord) => IO ()
+ensureIsUser :: forall user userId. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, FrameworkConfig, HasNewSessionUrl user, HasField "id" user userId, Typeable user, user ~ CurrentUserRecord) => IO ()
 ensureIsUser =
     case currentUserOrNothing @user of
         Just _ -> pure ()
-        Nothing -> TurboHaskell.LoginSupport.Types.throwNotLoggedIn (Just (newSessionUrl (Proxy :: Proxy user)))
+        Nothing -> redirectToLoginWithMessage (newSessionUrl (Proxy :: Proxy user))
 
 {-# INLINE currentAdmin #-}
-currentAdmin :: forall admin. (?controllerContext :: ControllerContext, HasNewSessionUrl admin, Typeable admin) => admin
-currentAdmin = fromMaybe (throwNotLoggedIn (pure (newSessionUrl (Proxy @admin)))) currentAdminOrNothing
+currentAdmin :: forall admin. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, FrameworkConfig, HasNewSessionUrl admin, Typeable admin) => admin
+currentAdmin = fromMaybe (redirectToLogin (newSessionUrl (Proxy @admin))) currentAdminOrNothing
 
 {-# INLINE currentAdminOrNothing #-}
-currentAdminOrNothing :: forall admin. (?controllerContext :: ControllerContext, HasNewSessionUrl admin, Typeable admin) => (Maybe admin)
+currentAdminOrNothing :: forall admin. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, FrameworkConfig, HasNewSessionUrl admin, Typeable admin) => (Maybe admin)
 currentAdminOrNothing = fromControllerContext @(Maybe admin)
 
 {-# INLINE currentAdminId #-}
-currentAdminId :: forall admin adminId. (?controllerContext :: ControllerContext, HasNewSessionUrl admin, HasField "id" admin adminId, Typeable admin) => adminId
+currentAdminId :: forall admin adminId. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, FrameworkConfig, HasNewSessionUrl admin, HasField "id" admin adminId, Typeable admin) => adminId
 currentAdminId = currentAdmin @admin |> get #id
 
 {-# INLINE ensureIsAdmin #-}
-ensureIsAdmin :: forall admin adminId. (?controllerContext :: ControllerContext, HasNewSessionUrl admin, Typeable admin) => IO ()
+ensureIsAdmin :: forall admin adminId. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, FrameworkConfig, HasNewSessionUrl admin, Typeable admin) => IO ()
 ensureIsAdmin =
     case currentAdminOrNothing @admin of
         Just _ -> pure ()
-        Nothing -> TurboHaskell.LoginSupport.Types.throwNotLoggedIn (Just (newSessionUrl (Proxy :: Proxy admin)))
+        Nothing -> redirectToLoginWithMessage (newSessionUrl (Proxy :: Proxy admin))
 
 -- Log's in an entity
 -- Examples:
@@ -61,14 +64,27 @@ ensureIsAdmin =
 -- let admin :: Admin = ... in login admin
 -- ```
 {-# INLINE login #-}
-login :: forall user id. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, KnownSymbol (ModelSupport.GetModelName user), HasField "id" user id, Show id) => user -> IO ()
+login :: forall user id. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, ?requestContext :: RequestContext, KnownSymbol (ModelSupport.GetModelName user), HasField "id" user id, Show id) => user -> IO ()
 login user = Session.setSession (sessionKey @user) (tshow (get #id user))
 
 -- Log's out an entity
 {-# INLINE logout #-}
-logout :: forall user id. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, KnownSymbol (ModelSupport.GetModelName user), HasField "id" user id, Show id) => user -> IO ()
+logout :: forall user id. (?controllerContext :: ControllerContext, ?requestContext :: RequestContext, ?requestContext :: RequestContext, KnownSymbol (ModelSupport.GetModelName user), HasField "id" user id, Show id) => user -> IO ()
 logout user = Session.setSession (sessionKey @user) ""
 
 {-# INLINE sessionKey #-}
 sessionKey :: forall user. (KnownSymbol (ModelSupport.GetModelName user)) => Text
 sessionKey = "login." <> ModelSupport.getModelName @user
+
+redirectToLoginWithMessage :: (?requestContext :: RequestContext, FrameworkConfig) => Text -> IO ()
+redirectToLoginWithMessage newSessionPath = do 
+    setSuccessMessage "Please log in to access this page"
+    setSession "TurboHaskell.LoginSupport.redirectAfterLogin" (cs getRequestUrl)
+    redirectToPath newSessionPath
+    error "Unreachable"
+
+
+redirectToLogin :: (?requestContext :: RequestContext, FrameworkConfig) => Text -> a
+redirectToLogin newSessionPath = unsafePerformIO $ do
+    redirectToPath newSessionPath
+    error "Unreachable"
--- TurboHaskell/LoginSupport/Middleware.hs
+++ TurboHaskell/LoginSupport/Middleware.hs
@@ -1,12 +1,11 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
-module TurboHaskell.LoginSupport.Middleware (middleware, initAuthentication) where
+module TurboHaskell.LoginSupport.Middleware (initAuthentication) where
 
 import ClassyPrelude hiding (catch)
 import           Control.Exception
 import Network.Wai (Application, Middleware, ResponseReceived)
 import TurboHaskell.ApplicationContext
-import qualified TurboHaskell.LoginSupport.Controller
 import TurboHaskell.LoginSupport.Types
 import qualified TurboHaskell.ControllerSupport as ControllerSupport
 import TurboHaskell.FrameworkConfig (FrameworkConfig)
@@ -21,18 +20,6 @@ import TurboHaskell.ControllerSupport
 import GHC.Records
 import GHC.TypeLits
 import Database.PostgreSQL.Simple (FromRow)
-
-middleware :: FrameworkConfig => ApplicationContext -> Middleware
-middleware applicationContext application request respond = (application request respond) `catch` (\(e :: NotLoggedIn) -> handleNotLoggedIn e)
-    where
-        handleNotLoggedIn ::  TurboHaskell.LoginSupport.Types.NotLoggedIn -> IO ResponseReceived
-        handleNotLoggedIn (TurboHaskell.LoginSupport.Types.NotLoggedIn newSessionUrl) = do
-            requestContext <- ControllerSupport.createRequestContext applicationContext request respond
-            let ?requestContext = requestContext
-            let ?applicationContext = applicationContext
-            let ?controllerContext = ControllerSupport.emptyControllerContext
-            let ?modelContext = modelContext ?applicationContext
-            ControllerSupport.runAction NotLoggedInAction { newSessionUrl }
 
 {-# INLINE initAuthentication #-}
 initAuthentication :: forall user.
