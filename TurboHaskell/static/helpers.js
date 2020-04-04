c3fd9758a182232b98a6615d096fd818ab9b29d4
Marc Scholten <marc@digitallyinduced.com>
2020-04-04 22:20:56 +0200

    FIxed svg issue

--- TurboHaskell/static/helpers.js
+++ TurboHaskell/static/helpers.js
@@ -297,8 +297,6 @@ window.transitionToNewPage = function (newBody) {
                 return false;
             } else if (isModalOpen && from.id === 'main-row') {
                 return false;
-            } else if (from.parentNode && from.parentNode.tagName === 'svg') {
-                return false;
             } else if (from.classList.contains('flatpickr-input') && from._flatpickr) {
                 unsafeSetTimeout(function (from, to) {
                     console.log('FROM', from, to.getAttribute('value'), to.value);
