151911d22fe326804fa8bfba5c3b4eeb6f4223b5
Marc Scholten <marc@digitallyinduced.com>
2020-04-27 15:56:48 +0200

    Added colors

--- TurboHaskell/IDE/SchemaDesigner/View/Tables/New.hs
+++ TurboHaskell/IDE/SchemaDesigner/View/Tables/New.hs
@@ -15,7 +15,7 @@ instance View NewTableView ViewContext where
             <form class="w-100 d-flex justify-content-end" action={pathTo PushToDbAction}>
                 <button type="submit" class="btn btn-primary my-3">Push to DB</button>
             </form>
-            <div class="row no-gutters">
+            <div class="row no-gutters bg-white">
                 {renderObjectSelector statements Nothing}
             </div>
         </div>
