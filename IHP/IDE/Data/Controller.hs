239c4fccbeebe211d75a458e779cbebcaf7f8ed7
Marc Scholten <marc@digitallyinduced.com>
2020-05-17 16:11:19 +0200

    Added first draft of loading data dynamically

--- IHP/IDE/Data/Controller.hs
+++ IHP/IDE/Data/Controller.hs
@@ -5,6 +5,25 @@ import IHP.IDE.ToolServer.Types
 import IHP.IDE.ToolServer.ViewContext
 import IHP.IDE.Data.View.ShowDatabase
 
+import qualified Database.PostgreSQL.Simple as PG
+import qualified Database.PostgreSQL.Simple.FromField as PG
+import qualified IHP.FrameworkConfig as Config
+
 instance Controller DataController where
     action ShowDatabaseAction = do
+        databaseUrl <- Config.appDatabaseUrl
+        connection <- PG.connectPostgreSQL databaseUrl
+        values :: [[DynamicField]] <- PG.query_ connection "SELECT * FROM users"
+        putStrLn (tshow values)
+        PG.close connection
         render ShowDatabaseView
+
+data DynamicField = DynamicField
+    { fieldValue :: Maybe ByteString
+    , fieldName :: ByteString
+    } deriving (Show)
+
+instance PG.FromField DynamicField where
+    fromField field fieldValue = pure DynamicField { .. }
+        where
+            fieldName = fromMaybe "" (PG.name field)
