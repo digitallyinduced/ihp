664e631bcaee4c0d6e44de507765c26668d3411d
Marc Scholten <marc@digitallyinduced.com>
2020-06-17 08:01:02 +0200

    ihp-new now dependens on git in nix store instead of relying in impure git in the env

--- ProjectGenerator/default.nix
+++ ProjectGenerator/default.nix
@@ -4,7 +4,9 @@ stdenv.mkDerivation rec {
   name = "ihp-new";
   src = ./.;
   installPhase = ''
-  	mkdir -p $out/bin;
-  	cp bin/ihp-new $out/bin;
+    mkdir -p $out/bin;
+    cp bin/ihp-new $out;
+    makeWrapper $out/ihp-new $out/bin/ihp-new --prefix PATH ":" "${git}/bin";
   '';
+  buildInputs = [ git makeWrapper ];
 }
