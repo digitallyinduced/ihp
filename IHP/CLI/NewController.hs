000694a1651eab4575985503f8ae6c7076c2f1ce
laobiz <pothjonas@gmail.com>
2020-07-17 08:47:27 +0200

    Use old app.controller nameing for new-controller cli

--- IHP/CLI/NewController.hs
+++ IHP/CLI/NewController.hs
@@ -7,33 +7,43 @@ import qualified System.Posix.Env.ByteString as Posix
 import Control.Monad.Fail
 import IHP.IDE.CodeGen.ControllerGenerator
 import IHP.IDE.CodeGen.Controller (executePlan)
-import Data.Text.Encoding as Text
+import qualified Data.Text as Text
 
 main :: IO ()
 main = do
     ensureIsInAppDirectory
 
-    args <- map Text.decodeUtf8 <$> Posix.getArgs
-    case args of
-        [] -> usage
-        [applicationName, controllerName] -> do
-            planOrError <- buildPlan controllerName applicationName
-            case planOrError of
-                Left error -> putStrLn error
-                Right plan -> executePlan plan
-        [controllerName] -> do
-            planOrError <- buildPlan controllerName "Web"
-            case planOrError of
-                Left error -> putStrLn error
-                Right plan -> executePlan plan
-        _ -> usage
-
+    args <- map cs <$> Posix.getArgs
+    case headMay args of
+        Just "" -> usage
+        Just appAndControllerName -> do
+            generateController appAndControllerName
 
 usage :: IO ()
-usage = putStrLn "Usage: new-controller appName controllerName \n new-controller controllerName"
+usage = putStrLn "Usage: new-controller RESOURCE_NAME"
 
 
 ensureIsInAppDirectory :: IO ()
 ensureIsInAppDirectory = do
     mainHsExists <- Directory.doesFileExist "Main.hs"
     unless mainHsExists (fail "You have to be in a project directory to run the generator")
+
+normalizeAppAndControllerName :: Text -> [Text]
+normalizeAppAndControllerName appAndControllerName = Text.splitOn "." appAndControllerName
+
+generateController :: Text -> IO ()
+generateController appAndControllerName = do
+    case Text.splitOn "." appAndControllerName of
+        [controllerName] -> do
+            planOrError <- buildPlan controllerName "Web"
+            case planOrError of
+                Left error -> putStrLn error
+                Right plan -> executePlan plan
+        [applicationName, controllerName] -> do
+            planOrError <- buildPlan controllerName applicationName
+            case planOrError of
+                Left error -> putStrLn error
+                Right plan -> executePlan plan
+        _ -> usage
+
+    
