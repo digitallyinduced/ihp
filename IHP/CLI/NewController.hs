35cc3e74b45634b7044b7d63ba7c2389547304fd
laobiz <pothjonas@gmail.com>
2020-07-17 07:47:39 +0200

    Changed buildPlan to new type in new-controller

--- IHP/CLI/NewController.hs
+++ IHP/CLI/NewController.hs
@@ -7,16 +7,22 @@ import qualified System.Posix.Env.ByteString as Posix
 import Control.Monad.Fail
 import IHP.IDE.CodeGen.ControllerGenerator
 import IHP.IDE.CodeGen.Controller (executePlan)
+import Data.Text.Encoding as Text
 
 main :: IO ()
 main = do
     ensureIsInAppDirectory
 
-    args <- map cs <$> Posix.getArgs
-    case headMay args of
-        Just "" -> usage
-        Just appAndControllerName -> do
-            planOrError <- buildPlan appAndControllerName
+    args <- map Text.decodeUtf8 <$> Posix.getArgs
+    case args of
+        [] -> usage
+        [applicationName, controllerName] -> do
+            planOrError <- buildPlan controllerName applicationName
+            case planOrError of
+                Left error -> putStrLn error
+                Right plan -> executePlan plan
+        [controllerName] -> do
+            planOrError <- buildPlan controllerName "Web"
             case planOrError of
                 Left error -> putStrLn error
                 Right plan -> executePlan plan
@@ -24,7 +30,7 @@ main = do
 
 
 usage :: IO ()
-usage = putStrLn "Usage: new-controller RESOURCE_NAME"
+usage = putStrLn "Usage: new-controller appName controllerName \n new-controller controllerName"
 
 
 ensureIsInAppDirectory :: IO ()
