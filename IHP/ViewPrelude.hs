d7850d8d316ea1ce451108bda8aac9d0bd363308
Marc Scholten <marc@digitallyinduced.com>
2020-08-22 16:20:49 +0200

    Added aeson to ViewPrelude

--- IHP/ViewPrelude.hs
+++ IHP/ViewPrelude.hs
@@ -19,6 +19,7 @@ module IHP.ViewPrelude (
     module IHP.ModelSupport,
     (!),
     module Data.Data,
+    module Data.Aeson
 ) where
 
 import IHP.Prelude
@@ -36,3 +37,4 @@ import IHP.Controller.RequestContext
 import IHP.RouterSupport
 import IHP.ModelSupport
 import Data.Data
+import Data.Aeson
