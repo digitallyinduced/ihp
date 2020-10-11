815cfd36dcd684baef73af10f779afb4248c0258
Marc Scholten <marc@digitallyinduced.com>
2020-10-11 15:09:06 +0200

    Added tests for NameSupport module

---
+++ Test/NameSupportSpec.hs
@@ -0,0 +1,100 @@
+{-|
+Module: Test.NameSupportSpec
+Copyright: (c) digitally induced GmbH, 2020
+-}
+module Test.NameSupportSpec where
+
+import Test.Hspec
+import IHP.Prelude
+import IHP.NameSupport
+
+
+tests = do
+    describe "NameSupport" do
+        describe "tableNameToModelName" do
+            it "should deal with empty input" do
+                tableNameToModelName "" `shouldBe` ""
+
+            it "should transform table names to model names" do 
+                tableNameToModelName "users" `shouldBe` "User"
+                tableNameToModelName "projects" `shouldBe` "Project"
+                tableNameToModelName "user_projects" `shouldBe` "UserProject"
+                tableNameToModelName "users_projects" `shouldBe` "UsersProject"
+                tableNameToModelName "people" `shouldBe` "Person"
+
+        describe "tableNameToControllerName" do
+            it "should deal with empty input" do
+                tableNameToControllerName "" `shouldBe` ""
+
+            it "should transform table names to controller names" do 
+                tableNameToControllerName "users" `shouldBe` "Users"
+                tableNameToControllerName "projects" `shouldBe` "Projects"
+                tableNameToControllerName "user_projects" `shouldBe` "UserProjects"
+                tableNameToControllerName "users_projects" `shouldBe` "UsersProjects"
+                tableNameToControllerName "people" `shouldBe` "People"
+        
+        describe "modelNameToTableName" do
+            it "should deal with empty input" do
+                modelNameToTableName "" `shouldBe` ""
+
+            it "should transform model names to table names" do 
+                modelNameToTableName "User" `shouldBe` "users"
+                modelNameToTableName "Project" `shouldBe` "projects"
+                modelNameToTableName "UserProject" `shouldBe` "user_projects"
+                modelNameToTableName "UsersProjects" `shouldBe` "users_projects"
+                modelNameToTableName "Person" `shouldBe` "persons"
+        
+        describe "columnNameToFieldName" do
+            it "should deal with empty input" do
+                columnNameToFieldName "" `shouldBe` ""
+
+            it "should transform column names to field names" do 
+                columnNameToFieldName "email" `shouldBe` "email"
+                columnNameToFieldName "project_id" `shouldBe` "projectId"
+                columnNameToFieldName "user_project_name" `shouldBe` "userProjectName"
+        
+        describe "fieldNameToColumnName" do
+            it "should deal with empty input" do
+                fieldNameToColumnName "" `shouldBe` ""
+
+            it "should transform field names to column names" do 
+                fieldNameToColumnName "email" `shouldBe` "email"
+                fieldNameToColumnName "projectId" `shouldBe` "project_id"
+                fieldNameToColumnName "userProjectName" `shouldBe` "user_project_name"
+        
+        describe "lcfirst" do
+            it "should deal with empty input" do
+                lcfirst "" `shouldBe` ""
+
+            it "should lowercase the first letter" do 
+                lcfirst "Hello" `shouldBe` "hello"
+                lcfirst "Hello World" `shouldBe` "hello World"
+                lcfirst "1337" `shouldBe` "1337"
+        
+        describe "ucfirst" do
+            it "should deal with empty input" do
+                ucfirst "" `shouldBe` ""
+
+            it "should uppercase the first letter" do 
+                ucfirst "hello" `shouldBe` "Hello"
+                ucfirst "hello world" `shouldBe` "Hello world"
+                ucfirst "1337" `shouldBe` "1337"
+
+        describe "escapeHaskellKeyword" do
+            it "should deal with empty input" do
+                escapeHaskellKeyword "" `shouldBe` ""
+
+            it "should escape haskell keywords" do
+                escapeHaskellKeyword "type" `shouldBe` "type_"
+                escapeHaskellKeyword "DATA" `shouldBe` "DATA_"
+
+            it "should ignore non-haskell keywords" do
+                escapeHaskellKeyword "hello" `shouldBe` "hello"
+
+        describe "toSlug" do
+            it "should deal with empty input" do
+                toSlug "" `shouldBe` ""
+
+            it "should make a slug string from a string" do
+                toSlug "IHP Release: 21.08.2020 (v21082020)" `shouldBe` "ihp-release-21-08-2020-v21082020"
+                toSlug "Hallo! @ Welt" `shouldBe` "hallo-welt"
