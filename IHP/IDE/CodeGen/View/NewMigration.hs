c571bd6f931586734ca973261c441792a1f0b72a
Marc Scholten <marc@digitallyinduced.com>
2020-11-08 17:55:34 +0100

    Added new migration system for migrating production database schemas

---
+++ IHP/IDE/CodeGen/View/NewMigration.hs
@@ -0,0 +1,38 @@
+module IHP.IDE.CodeGen.View.NewMigration where
+
+import IHP.ViewPrelude
+import IHP.IDE.SchemaDesigner.Types
+import IHP.IDE.ToolServer.Types
+import IHP.IDE.ToolServer.Layout
+import IHP.IDE.SchemaDesigner.View.Layout
+import IHP.IDE.CodeGen.Types
+import IHP.IDE.CodeGen.View.Generators (renderPlan)
+
+data NewMigrationView = NewMigrationView { description :: Text }
+
+instance View NewMigrationView where
+    html NewMigrationView { .. } = [hsx|
+        <div class="generators">
+            {renderFlashMessages}
+            <div class="container pt-5">
+                <div class="code-generator new-script">
+                    {renderForm}
+                </div>
+            </div>
+        </div>
+    |]
+        where
+            renderForm = [hsx|
+                <form method="POST" action={CreateMigrationAction} class="d-flex">
+                    <input
+                        type="text"
+                        name="description"
+                        placeholder="Migration description"
+                        class="form-control"
+                        autofocus="autofocus"
+                        value={description}
+                        />
+
+                    <button class="btn btn-primary" type="submit">Generate</button>
+                </form>
+            |]
