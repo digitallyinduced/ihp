aa961a8d5d17470db727ba4104f59da5f65c25bd
Marc Scholten <marc@digitallyinduced.com>
2020-12-24 10:45:35 +0100

    Added new API for managing the project's nixpkgs-config.nix

---
+++ NixSupport/make-nixpkgs-from-options.nix
@@ -0,0 +1,70 @@
+# This function is imported from the apps Config/nix/nixpkgs-config.nix
+# It takes a few parameter as an input and returns a nixpkgs version used for building
+# the project
+{ ihp
+, haskellPackagesDir
+, dontCheckPackages ? []
+, doJailbreakPackages ? []
+, dontHaddockPackages ? []
+, nixPkgsRev ? "2ae527c50e049570dd25132f9325527aa2320e32"
+, nixPkgsSha256 ? "0rggj7d5j6mmn5l4p72pj0bdxcsid0wxyjqfivdhmfvlsmh8hadn"
+, compiler ? "ghc8102"
+, manualOverrides ? haskellPackagesNew: haskellPackagesOld: { } # More exotic overrides go here
+}:
+let
+  generatedOverrides = haskellPackagesNew: haskellPackagesOld:
+    let
+      toPackage = dir: file: _: {
+        name = builtins.replaceStrings [ ".nix" ] [ "" ] file;
+
+        value = haskellPackagesNew.callPackage ("${dir}/${file}") {};
+      };
+      makePackageSet = dir: pkgs.lib.mapAttrs' (toPackage dir) (builtins.readDir dir);
+    in
+      { "ihp" = ((haskellPackagesNew.callPackage "${ihp}/ihp.nix") { }); } // (makePackageSet haskellPackagesDir) // (makePackageSet "${ihp}/NixSupport/haskell-packages/.");
+
+  makeOverrides =
+    function: names: haskellPackagesNew: haskellPackagesOld:
+      let
+        toPackage = name: {
+          inherit name;
+
+          value = function haskellPackagesOld.${name};
+        };
+      in
+      builtins.listToAttrs (map toPackage names);
+
+  composeExtensionsList = pkgs.lib.fold pkgs.lib.composeExtensions (_: _: {});
+
+  config = {
+    allowBroken = true;
+    packageOverrides = pkgs: rec {
+      haskell = pkgs.haskell // {
+        packages = pkgs.haskell.packages // {
+          "${compiler}" =
+          pkgs.haskell.packages."${compiler}".override {
+            overrides = composeExtensionsList [
+              generatedOverrides
+              (makeOverrides pkgs.haskell.lib.dontCheck   dontCheckPackages  )
+              (makeOverrides pkgs.haskell.lib.doJailbreak doJailbreakPackages)
+              (makeOverrides pkgs.haskell.lib.dontHaddock dontHaddockPackages)
+              manualOverrides
+            ];
+          };
+        }
+        ;
+      }
+      ;
+    };
+  };
+
+
+  pkgs = (import ((import <nixpkgs> {}).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = nixPkgsRev;
+    sha256 = nixPkgsSha256;
+  })) { inherit config; };
+
+in
+    pkgs
