a3b59f5e7bdfddd11de1761ea9da126058edc623
Alexander-Block <47148212+Alexander-Block@users.noreply.github.com>
2021-03-18 19:54:23 +0100

    move isIllegalKeyword from VIew to Controller

--- IHP/IDE/SchemaDesigner/Controller/Columns.hs
+++ IHP/IDE/SchemaDesigner/Controller/Columns.hs
@@ -11,7 +11,7 @@ import IHP.IDE.SchemaDesigner.View.Columns.EditForeignKey
 import IHP.IDE.SchemaDesigner.Parser
 import IHP.IDE.SchemaDesigner.Compiler
 import IHP.IDE.SchemaDesigner.Types
-import IHP.IDE.SchemaDesigner.View.Layout (findStatementByName, findStatementByName, removeQuotes, replace, getDefaultValue, isIllegalKeyword, findForeignKey)
+import IHP.IDE.SchemaDesigner.View.Layout (findStatementByName, findStatementByName, removeQuotes, replace, getDefaultValue, findForeignKey)
 import qualified IHP.SchemaCompiler as SchemaCompiler
 import qualified System.Process as Process
 import IHP.IDE.SchemaDesigner.Parser (schemaFilePath)
@@ -19,6 +19,7 @@ import qualified Data.Text.IO as Text
 import IHP.IDE.SchemaDesigner.Controller.Schema
 import IHP.IDE.SchemaDesigner.Controller.Helper
 import IHP.IDE.SchemaDesigner.View.Layout
+import IHP.IDE.SchemaDesigner.Controller.Validation
 
 instance Controller ColumnsController where
     beforeAction = setLayout schemaDesignerLayout
