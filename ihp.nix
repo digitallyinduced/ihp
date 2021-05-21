5127e30ab23048c4e074f5dc649a9a7da443874e
Marc Scholten <marc@digitallyinduced.com>
2021-05-21 19:18:57 +0200

    Added new v0.10.0 version number

--- ihp.nix
+++ ihp.nix
@@ -57,7 +57,7 @@
 }:
 mkDerivation {
   pname = "ihp";
-  version = "v0.8.0";
+  version = "v0.10.0";
   src = (import <nixpkgs> { }).nix-gitignore.gitignoreSource [ ] ./.;
   isLibrary = true;
   isExecutable = true;
