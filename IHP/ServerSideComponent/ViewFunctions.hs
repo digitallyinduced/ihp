faf0e5d8fc65d138cce8001abb951584f6bb0b2b
Marc Scholten <marc@digitallyinduced.com>
2021-06-13 13:20:33 +0200

    Added componentFromState

--- IHP/ServerSideComponent/ViewFunctions.hs
+++ IHP/ServerSideComponent/ViewFunctions.hs
@@ -8,17 +8,19 @@ module IHP.ServerSideComponent.ViewFunctions where
 import IHP.Prelude
 import IHP.ViewSupport
 import IHP.ServerSideComponent.Types
-import IHP.HtmlSupport.QQ (hsx)
+import IHP.HSX.QQ (hsx)
 import qualified Text.Blaze.Html5 as Html5
 import IHP.Controller.Context
 
 import qualified Data.Typeable as Typeable
 
 component :: forall component action. (Component component action, Typeable component) => Html
-component = [hsx|<div class="ihp-ssc" data-url={url}>{inner}</div>|]
+component = componentFromState (initialState @component)
+
+componentFromState :: forall component action. (Component component action, Typeable component) => state -> Html
+componentFromState state = [hsx|<div class="ihp-ssc" data-path={path}>{render state}</div>|]
     where
-        inner = render (initialState @component)
-        url = "ws://localhost:8000/SSC/" <> typeName
+        path = "/SSC/" <> typeName
         typeName = (undefined :: component)
                 |> Typeable.typeOf
                 |> show
