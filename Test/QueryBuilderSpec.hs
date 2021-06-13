937ea5103e306ef277dd18a35ddfd65b712b3b01
dlahm <david@klinger-lahm.de>
2021-06-13 12:34:54 +0200

    indexResult replaced with labelResult in test.

--- Test/QueryBuilderSpec.hs
+++ Test/QueryBuilderSpec.hs
@@ -192,11 +192,11 @@ tests = do
                 (toSQL theQuery) `shouldBe` ("SELECT posts.* FROM posts INNER JOIN users ON posts.created_by = users.id INNER JOIN favorite_title ON posts.title = favorite_title.title WHERE users.name != ?", [Escape "Tom"])
 
 
-        describe "indexResults" do
+        describe "labelResults" do
             it "should provide a query with index field" do
                 let theQuery = query @Post
                         |> innerJoin @User (#createdBy, #id)
-                        |> indexResults @User #id
+                        |> labelResults @User #id
                 (toSQL theQuery) `shouldBe` ("SELECT users.id, posts.* FROM posts INNER JOIN users ON posts.created_by = users.id", [])
 
 
