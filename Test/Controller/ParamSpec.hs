69db7d2c147a12d8b9e814e78d264c8a90f5a09c
Nikolay Yakimov <root@livid.pp.ru>
2021-08-03 22:28:51 +0300

    Remove trailing spaces

--- Test/Controller/ParamSpec.hs
+++ Test/Controller/ParamSpec.hs
@@ -23,15 +23,15 @@ tests = do
             it "should parse valid input" do
                 let ?context = createControllerContextWithParams [("page", "1")]
                 (param @Int "page") `shouldBe` 1
-            
+
             it "should fail on empty input" do
                 let ?context = createControllerContextWithParams [("page", "")]
                 (IO.evaluate (param @Int "page")) `shouldThrow` (== ParamCouldNotBeParsedException { name = "page", parserError = "ParamReader Int: not enough input" })
-            
+
             it "should fail if param not provided" do
                 let ?context = createControllerContextWithParams []
                 (IO.evaluate (param @Int "page")) `shouldThrow` (== ParamNotFoundException { name = "page" })
-            
+
             it "should fail with a parser error on invalid input" do
                 let ?context = createControllerContextWithParams [("page", "NaN")]
                 (IO.evaluate (param @Int "page")) `shouldThrow` (== ParamCouldNotBeParsedException { name = "page", parserError = "ParamReader Int: Failed reading: takeWhile1" })
@@ -40,15 +40,15 @@ tests = do
             it "should parse valid input" do
                 let ?context = createControllerContextWithParams [("referredBy", "776ab71d-327f-41b3-90a8-7b5a251c4b88")]
                 (paramOrNothing @UUID "referredBy") `shouldBe` (Just "776ab71d-327f-41b3-90a8-7b5a251c4b88")
-            
+
             it "should return Nothing on empty input" do
                 let ?context = createControllerContextWithParams [("referredBy", "")]
                 (paramOrNothing @UUID "referredBy") `shouldBe` Nothing
-            
+
             it "should return Nothing if param not provided" do
                 let ?context = createControllerContextWithParams []
                 (paramOrNothing @UUID "referredBy") `shouldBe` Nothing
-            
+
             it "should fail with a parser error on invalid input" do
                 let ?context = createControllerContextWithParams [("referredBy", "not a uuid")]
                 (IO.evaluate (paramOrNothing @UUID "referredBy")) `shouldThrow` (== ParamCouldNotBeParsedException { name = "referredBy", parserError = "FromParameter UUID: Parse error" })
@@ -57,15 +57,15 @@ tests = do
             it "should parse valid input" do
                 let ?context = createControllerContextWithParams [("page", "1")]
                 (paramOrDefault @Int 0 "page") `shouldBe` 1
-            
+
             it "should return default value on empty input" do
                 let ?context = createControllerContextWithParams [("page", "")]
                 (paramOrDefault @Int 10 "page") `shouldBe` 10
-            
+
             it "should return default value if param not provided" do
                 let ?context = createControllerContextWithParams []
                 (paramOrDefault @Int 10 "page") `shouldBe` 10
-            
+
             it "should fail with a parser error on invalid input" do
                 let ?context = createControllerContextWithParams [("page", "NaN")]
                 (IO.evaluate (paramOrDefault @Int 10 "page")) `shouldThrow` (== ParamCouldNotBeParsedException { name = "page", parserError = "ParamReader Int: Failed reading: takeWhile1" })
@@ -75,11 +75,11 @@ tests = do
             it "should parse valid input" do
                 let ?context = createControllerContextWithParams [("ingredients", "milk"), ("ingredients", "egg")]
                 (paramList @Text "ingredients") `shouldBe` ["milk", "egg"]
-            
+
             it "should fail on invalid input" do
                 let ?context = createControllerContextWithParams [("numbers", "1"), ("numbers", "NaN")]
                 (IO.evaluate (paramList @Int "numbers")) `shouldThrow` (errorCall "param: Parameter 'numbers' is invalid")
-            
+
             it "should deal with empty input" do
                 let ?context = createControllerContextWithParams []
                 (paramList @Int "numbers") `shouldBe` []
@@ -88,11 +88,11 @@ tests = do
             it "returns True if param given" do
                 let ?context = createControllerContextWithParams [("a", "test")]
                 hasParam "a" `shouldBe` True
-            
+
             it "returns True if param given but empty" do
                 let ?context = createControllerContextWithParams [("a", "")]
                 hasParam "a" `shouldBe` True
-            
+
             it "returns False if param missing" do
                 let ?context = createControllerContextWithParams []
                 hasParam "a" `shouldBe` False
@@ -103,33 +103,33 @@ tests = do
                     (readParameter @ByteString "test") `shouldBe` (Right "test")
                 it "should handle JSON strings" do
                     (readParameterJSON @ByteString (json "\"test\"")) `shouldBe` (Right ("test" :: ByteString))
-                
+
                 it "should fail on other JSON input" do
                     (readParameterJSON @ByteString (json "1")) `shouldBe` (Left ("ParamReader ByteString: Expected String" :: ByteString))
-            
+
             describe "Int" do
                 it "should accept numeric input" do
                     (readParameter @Int "1337") `shouldBe` (Right 1337)
-                
+
                 it "should accept negative numbers" do
                     (readParameter @Int "-1337") `shouldBe` (Right (-1337))
 
                 it "should accept JSON numerics " do
                     (readParameterJSON @Int (json "1337")) `shouldBe` (Right 1337)
-                
+
                 it "should fail on other JSON input " do
                     (readParameterJSON @Int (json "true")) `shouldBe` (Left "ParamReader Int: Expected Int")
 
             describe "Integer" do
                 it "should accept numeric input" do
                     (readParameter @Integer "1337") `shouldBe` (Right 1337)
-                
+
                 it "should accept negative numbers" do
                     (readParameter @Integer "-1337") `shouldBe` (Right (-1337))
 
                 it "should accept JSON numerics " do
                     (readParameterJSON @Integer (json "1337")) `shouldBe` (Right 1337)
-                
+
                 it "should fail on other JSON input " do
                     (readParameterJSON @Integer (json "true")) `shouldBe` (Left "ParamReader Integer: Expected Integer")
                     (readParameterJSON @Integer (json "\"1\"")) `shouldBe` (Left "ParamReader Integer: Expected Integer")
@@ -142,13 +142,13 @@ tests = do
                 it "should accept floating point input" do
                     (readParameter @Double "1.2") `shouldBe` (Right 1.2)
                     (readParameter @Float "1.2345679") `shouldBe` (Right 1.2345679)
-                
+
                 it "should accept JSON integer input" do
                     (readParameterJSON @Double (json "1337")) `shouldBe` (Right 1337)
 
                 it "should accept JSON floating point input" do
                     (readParameterJSON @Double (json "1.2")) `shouldBe` (Right 1.2)
-                
+
                 it "should fail on other JSON input " do
                     (readParameterJSON @Double (json "true")) `shouldBe` (Left "ParamReader Double: Expected Double")
                     (readParameterJSON @Double (json "\"1\"")) `shouldBe` (Left "ParamReader Double: Expected Double")
@@ -160,13 +160,13 @@ tests = do
                 it "should accept floating point input" do
                     (readParameter @Float "1.2") `shouldBe` (Right 1.2)
                     (readParameter @Float "1.2345679") `shouldBe` (Right 1.2345679)
-                
+
                 it "should accept JSON integer input" do
                     (readParameterJSON @Float (json "1337")) `shouldBe` (Right 1337)
 
                 it "should accept JSON floating point input" do
                     (readParameterJSON @Float (json "1.2")) `shouldBe` (Right 1.2)
-                
+
                 it "should fail on other JSON input " do
                     (readParameterJSON @Float (json "true")) `shouldBe` (Left "ParamReader Float: Expected Float")
                     (readParameterJSON @Float (json "\"1\"")) `shouldBe` (Left "ParamReader Float: Expected Float")
@@ -177,13 +177,13 @@ tests = do
 
                 it "should accept floating point input" do
                     (readParameter @Point "1.2,1.3") `shouldBe` (Right Point { x = 1.2, y = 1.3 })
-                
+
                 it "should accept JSON integer input" do
                     (readParameterJSON @Point (json "\"1337,1338\"")) `shouldBe` (Right Point { x = 1337, y = 1338 })
 
                 it "should accept JSON floating point input" do
                     (readParameterJSON @Point (json "\"1.2,1.3\"")) `shouldBe` (Right Point { x = 1.2, y = 1.3 })
-                
+
                 it "should fail on other JSON input " do
                     (readParameterJSON @Point (json "true")) `shouldBe` (Left "ParamReader Point: Expected Point")
                     (readParameterJSON @Point (json "\"1\"")) `shouldBe` (Left "ParamReader Point: ,: not enough input")
@@ -192,10 +192,10 @@ tests = do
             describe "Text" do
                 it "should handle text input" do
                     (readParameter @Text "test") `shouldBe` (Right "test")
-                
+
                 it "should handle JSON strings" do
                     (readParameterJSON @Text (json "\"test\"")) `shouldBe` (Right ("test"))
-                
+
                 it "should fail on other JSON input" do
                     (readParameterJSON @Text (json "1")) `shouldBe` (Left ("ParamReader Text: Expected String"))
 
@@ -205,16 +205,16 @@ tests = do
 
                 it "should handle a single value" do
                     (readParameter @[Int] "1") `shouldBe` (Right [1])
-                
+
                 it "should handle comma separated values" do
                     (readParameter @[Int] "1,2,3") `shouldBe` (Right [1,2,3])
 
                 it "should fail if a single value is invalid" do
                     (readParameter @[Int] "1,a,3") `shouldBe` (Left "ParamReader Int: Failed reading: takeWhile1")
-                
+
                 it "should handle JSON arrays" do
                     (readParameterJSON @[Int] (json "[1,2,3]")) `shouldBe` (Right [1,2,3])
-                
+
                 it "should fail on JSON input that is not an array" do
                     (readParameterJSON @[Int] (json "true")) `shouldBe` (Left "ParamReader Text: Expected Array")
 
@@ -241,7 +241,7 @@ tests = do
 
                 it "should accept JSON UUIDs" do
                     (readParameterJSON @UUID (json "\"6188329c-6bad-47f6-800c-2fd19ce0b2df\"")) `shouldBe` (Right "6188329c-6bad-47f6-800c-2fd19ce0b2df")
-                
+
                 it "should fail on invalid JSON input" do
                     (readParameterJSON @UUID (json "\"not a uuid\"")) `shouldBe` (Left "FromParameter UUID: Parse error")
                     (readParameterJSON @UUID (json "false")) `shouldBe` (Left "ParamReader UUID: Expected String")
@@ -249,47 +249,47 @@ tests = do
             describe "UTCTime" do
                 it "should accept timestamps" do
                     (tshow (readParameter @UTCTime "2020-11-08T12:03:35Z")) `shouldBe` ("Right 2020-11-08 12:03:35 UTC")
-                
+
                 it "should accept dates" do
                     (tshow (readParameter @UTCTime "2020-11-08")) `shouldBe` ("Right 2020-11-08 00:00:00 UTC")
-                
+
                 it "should fail on invalid inputs" do
                     (readParameter @UTCTime "not a timestamp") `shouldBe` (Left "ParamReader UTCTime: Failed parsing")
-                
+
                 it "should accept JSON strings" do
                     (tshow (readParameterJSON @UTCTime (json "\"2020-11-08T12:03:35Z\""))) `shouldBe` ("Right 2020-11-08 12:03:35 UTC")
 
             describe "LocalTime" do
                 it "should accept timestamps" do
                     (tshow (readParameter @LocalTime "2020-11-08T12:03:35Z")) `shouldBe` ("Right 2020-11-08 12:03:35")
-                
+
                 it "should accept dates" do
                     (tshow (readParameter @LocalTime "2020-11-08")) `shouldBe` ("Right 2020-11-08 00:00:00")
-                
+
                 it "should fail on invalid inputs" do
                     (readParameter @LocalTime "not a timestamp") `shouldBe` (Left "ParamReader LocalTime: Failed parsing")
-                
+
                 it "should accept JSON strings" do
                     (tshow (readParameterJSON @LocalTime (json "\"2020-11-08T12:03:35Z\""))) `shouldBe` ("Right 2020-11-08 12:03:35")
 
             describe "Day" do
                 it "should accept dates" do
                     (tshow (readParameter @Day "2020-11-08")) `shouldBe` ("Right 2020-11-08")
-                
+
                 it "should fail on invalid inputs" do
                     (readParameter @Day "not a timestamp") `shouldBe` (Left "ParamReader Day: Failed parsing")
-                
+
                 it "should accept JSON strings" do
                     (tshow (readParameterJSON @Day (json "\"2020-11-08\""))) `shouldBe` ("Right 2020-11-08")
 
             describe "TimeOfDay" do
                 it "should accept time values" do
                     (tshow (readParameter @TimeOfDay "12:00:00")) `shouldBe` ("Right 12:00:00")
-                
+
                 it "should fail on invalid inputs" do
                     (readParameter @TimeOfDay "not a time") `shouldBe` (Left "ParamReader TimeOfDay: Please enter a valid time in the format hh:mm:ss")
                     (readParameter @TimeOfDay "25:00:00") `shouldBe` (Left "ParamReader TimeOfDay: Please enter a valid time in the format hh:mm:ss")
-                
+
                 it "should accept JSON strings" do
                     (tshow (readParameterJSON @TimeOfDay (json "\"13:37:00\""))) `shouldBe` ("Right 13:37:00")
 
@@ -301,7 +301,7 @@ tests = do
                 it "should handle empty input as Nothing" do
                     (readParameter @(Maybe Int) "") `shouldBe` (Right Nothing)
                     (readParameter @(Maybe UUID) "") `shouldBe` (Right Nothing)
-                
+
                 it "should handle empty Text as Just" do
                     (readParameter @(Maybe Text) "") `shouldBe` (Right (Just ""))
                     (readParameter @(Maybe ByteString) "") `shouldBe` (Right (Just ""))
--- Test/ModelSupportSpec.hs
+++ Test/ModelSupportSpec.hs
@@ -18,38 +18,38 @@ tests = do
                 it "should return the text" do
                     let text :: Text = "Lorem Ipsum"
                     (inputValue text) `shouldBe` text
-            
+
             describe "Int" do
                 it "should return numeric representation" do
                     (inputValue (1 :: Int)) `shouldBe` "1"
-            
+
             describe "Integer" do
                 it "should return numeric representation" do
                     (inputValue (1 :: Int)) `shouldBe` "1"
-            
+
             describe "Double" do
                 it "should return numeric representation" do
                     (inputValue (1.337 :: Double)) `shouldBe` "1.337"
-            
+
             describe "Float" do
                 it "should return numeric representation" do
                     (inputValue (1.337 :: Float)) `shouldBe` "1.337"
-            
+
             describe "Bool" do
                 it "should deal with True and False" do
                     (inputValue True) `shouldBe` "on"
                     (inputValue False) `shouldBe` "off"
-            
+
             describe "UUID" do
                 it "should return text representation" do
                     let uuid :: UUID = "b9a1129e-e53a-4370-b778-0d1c28dc6ecc"
                     (inputValue uuid) `shouldBe` "b9a1129e-e53a-4370-b778-0d1c28dc6ecc"
-            
+
             describe "UTCTime" do
                 it "should return text representation" do
                     let (Just utctime) :: Maybe UTCTime = parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%S%QZ" "2020-11-08T12:03:35Z"
                     (inputValue utctime) `shouldBe` "2020-11-08T12:03:35Z"
-            
+
             describe "Day" do
                 it "should return text representation" do
                     let (Just utctime) :: Maybe UTCTime = parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%S%QZ" "2020-11-08T12:03:35Z"
@@ -60,25 +60,25 @@ tests = do
                 it "should return text representation" do
                     let timeOfDay :: TimeOfDay = read "12:00:00"
                     (inputValue timeOfDay) `shouldBe` "12:00:00"
-            
+
             describe "Maybe" do
                 it "should return empty string on Nothing" do
                     let value :: Maybe Int = Nothing
                     (inputValue value) `shouldBe` ""
-                
+
                 it "should return the value if Just" do
                     let value :: Maybe Int = Just 1
                     (inputValue value) `shouldBe` "1"
-            
+
             describe "[a]" do
                 it "should return CSV" do
                     let value :: [Int] = [1, 2, 3]
                     (inputValue value) `shouldBe` "1,2,3"
-                
+
                 it "should return empty string on empty list" do
                     let value :: [Int] = []
                     (inputValue value) `shouldBe` ""
-            
+
             describe "JSON" do
                 it "should return a json string for a Aeson.Value" do
                     let (Just value) :: Maybe Aeson.Value = Aeson.decode "{\"hello\":true}"
@@ -86,7 +86,7 @@ tests = do
 
         describe "didChange" do
             let project = Project { id = 1337, name = "Test", meta = def { originalDatabaseRecord = Just (Dynamic.toDyn project) } }
-            
+
             it "should return False for a new record" do
                 (project |> didChange #name) `shouldBe` False
                 (project |> didChange #id) `shouldBe` False
@@ -94,7 +94,7 @@ tests = do
             it "should return True for a changed field" do
                 (project |> set #name "Changed" |> didChange #name) `shouldBe` True
                 (project |> didChange #id) `shouldBe` False
-            
+
             it "should return false for a changed field when set with the same value again" do
                 (project |> set #name "Test" |> didChange #name) `shouldBe` False
                 (project |> didChange #id) `shouldBe` False
