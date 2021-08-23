9eca4a96eafc1b1265161a7b47088a1fcc86ff17
Marc Scholten <marc@digitallyinduced.com>
2021-08-23 21:03:46 +0200

    Cleaned up github login controller

--- Plugins/ihp-oauth-github/IHP/OAuth/Github/Controller.hs
+++ Plugins/ihp-oauth-github/IHP/OAuth/Github/Controller.hs
@@ -7,6 +7,8 @@ import qualified IHP.OAuth.Github.GithubApi as Github
 import IHP.LoginSupport.Types
 import qualified IHP.OAuth.Github.Config as Config
 import qualified Data.TMap as TMap
+import qualified IHP.AuthSupport.Lockable as Lockable
+import qualified IHP.AuthSupport.Controller.Sessions as Sessions
 
 newSessionWithGithubAction :: forall user. (?context :: ControllerContext, HasPath Github.GithubOAuthController) => IO ()
 newSessionWithGithubAction = do
@@ -24,12 +26,24 @@ githubConnectCallbackAction :: forall user.
     , FromRow user
     , ?modelContext :: ModelContext
     , KnownSymbol (GetTableName user)
-    , HasField "githubUserId" user (Maybe Int)
     , GithubOAuthControllerConfig user
     , user ~ CurrentUserRecord
     , HasNewSessionUrl user
     , Typeable user
     , ?context :: ControllerContext
+    , SetField "githubUserId" user (Maybe Int)
+    , HasField "githubUserId" user (Maybe Int)
+    , SetField "email" user Text
+    , SetField "passwordHash" user Text
+    , Record user
+    , CanUpdate user
+    , SetField "failedLoginAttempts" user Int
+    , KnownSymbol (GetModelName user)
+    , HasField "id" user (Id user)
+    , Show (PrimaryKey (GetTableName user))
+    , Sessions.SessionsControllerConfig user
+    , HasField "lockedAt" user (Maybe UTCTime)
+    , CanCreate user
     ) => IO ()
 githubConnectCallbackAction = do
     let code = param @Text "code"
@@ -50,15 +64,32 @@ githubConnectCallbackAction = do
             when (isJust (get #githubUserId user)) do
                 setErrorMessage "You're already connected with a Github Account. Please remove the existing Github Account connection first."
                 redirectToPath (newSessionUrl (Proxy @user))
-
-            connectGithubWithExistingUser githubUser user accessToken -- User wants to connect Github to existing account
+            
+            user <- user
+                    |> setJust #githubUserId (get #id githubUser)
+                    |> \user -> beforeLogin user githubUser
+                    >>= updateRecord
+            
+            setSuccessMessage "Your GitHub Account has been linked to this account now"
+            redirectToPath (newSessionUrl (Proxy @user))
         Nothing -> do
             user <- query @user
                 |> filterWhere (#githubUserId, Just (get #id githubUser))
                 |> fetchOneOrNothing
 
             case user of
-                Just user -> loginExistingUser githubUser user accessToken
+                Just user -> do
+                    ensureIsNotLocked user
+                    Sessions.beforeLogin user
+                    login user
+                    
+                    user
+                            |> set #failedLoginAttempts 0
+                            |> \user -> beforeLogin user githubUser
+                            >>= updateRecord
+
+                    pure ()
+                    -- loginExistingUser githubUser user accessToken
                 Nothing -> do
                     githubUser <- case githubUser of
                         Github.GithubUser { .. } -> pure Github.GithubUser { .. }
@@ -74,16 +105,38 @@ githubConnectCallbackAction = do
                         |> filterWhere (#email, githubEmail)
                         |> fetchOneOrNothing
 
+                    randomPassword <- generateAuthenticationToken
+                    hashed <- hashPassword randomPassword
+
                     case userWithSameEmail of
                         Just user -> do
                             setErrorMessage "There's an existing user with the same email address already. Please log in with your password and then connect your GitHub account in the account settings."
                             redirectToPath (newSessionUrl (Proxy @user))
-                        Nothing -> loginNewUser @user githubUser accessToken
+                        Nothing -> do
+                            let user = newRecord @user
+                                    |> set #passwordHash hashed
+                                    |> set #email githubEmail
+                                    |> setJust #githubUserId (get #id githubUser)
+
+                            user <- createUser (beforeCreateUser user githubUser) githubUser
+                            afterCreateUser user
+                            login user
+
+            redirectUrl <- getSessionAndClear "IHP.LoginSupport.redirectAfterLogin"
+            redirectToPath (fromMaybe (Sessions.afterLoginRedirectPath @user) redirectUrl)
 
 class GithubOAuthControllerConfig user where
-    loginExistingUser :: (?context :: ControllerContext, ?modelContext :: ModelContext) => Github.GithubUser -> user -> Text -> IO ()
-    loginNewUser :: (?context :: ControllerContext, ?modelContext :: ModelContext) => Github.GithubUser -> Text -> IO ()
-    connectGithubWithExistingUser :: (?context :: ControllerContext, ?modelContext :: ModelContext) => Github.GithubUser -> user -> Text -> IO ()
+    createUser :: (?context :: ControllerContext, ?modelContext :: ModelContext, CanCreate user) => user -> Github.GithubUser -> IO user
+    createUser user githubUser = createRecord user
+    
+    beforeCreateUser :: (?context :: ControllerContext, ?modelContext :: ModelContext, CanCreate user) => user -> Github.GithubUser -> user
+    beforeCreateUser user githubUser = user
+
+    afterCreateUser :: (?context :: ControllerContext, ?modelContext :: ModelContext, CanCreate user) => user -> IO ()
+    afterCreateUser user = pure ()
+
+    beforeLogin :: (?context :: ControllerContext, ?modelContext :: ModelContext, CanCreate user) => user -> Github.GithubUser -> IO user
+    beforeLogin user githubUser = pure user
 
 
 githubOAuthConfig :: (?context :: ControllerContext) => Github.GithubOAuthConfig
@@ -94,3 +147,10 @@ githubOAuthConfig = ?context
             |> fromMaybe (error "Could not find GithubOAuthConfig in config. Did you forgot to call 'initGithubOAuth' inside your Config.hs?")
 
 
+
+ensureIsNotLocked :: forall user. (?context :: ControllerContext, HasNewSessionUrl user, HasField "lockedAt" user (Maybe UTCTime)) => user -> IO ()
+ensureIsNotLocked user = do
+    isLocked <- Lockable.isLocked user
+    when isLocked do
+        setErrorMessage "User is locked"
+        redirectToPath (newSessionUrl (Proxy @user))
