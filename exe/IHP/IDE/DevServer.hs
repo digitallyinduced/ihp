b50e7148fd15191aff2bb34803180e06ca97839b
Marc Scholten <marc@digitallyinduced.com>
2021-08-28 14:11:09 +0200

    Reduce memory usage by IHP dev server

--- exe/IHP/IDE/DevServer.hs
+++ exe/IHP/IDE/DevServer.hs
@@ -230,7 +230,7 @@ startGHCI = do
             , "-package-env -" -- Do not load `~/.ghc/arch-os-version/environments/name file`, global packages interfere with our packages
             , "-ignore-dot-ghci" -- Ignore the global ~/.ghc/ghci.conf That file sometimes causes trouble (specifically `:set +c +s`)
             , "-ghci-script", ".ghci" -- Because the previous line ignored default ghci config file locations, we have to manual load our .ghci
-            , "+RTS", "-A512m", "-n4m", "-H512m", "--nonmoving-gc", "-N"
+            , "+RTS", "-A128m", "-n2m", "-H2m", "--nonmoving-gc", "-N"
             ]
     createManagedProcess (Process.proc "ghci" args)
             { Process.std_in = Process.CreatePipe
