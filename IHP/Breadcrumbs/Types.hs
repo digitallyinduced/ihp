2d2134f0f4e351f3d8b5c33cbd9215c082e142e0
Amitai Burstein <amitai@gizra.com>
2021-10-26 23:28:46 +0300

    Implement instance Show BreadcrumbsItem

--- IHP/Breadcrumbs/Types.hs
+++ IHP/Breadcrumbs/Types.hs
@@ -2,6 +2,8 @@ module IHP.Breadcrumbs.Types where
 
 import IHP.Prelude
 import Text.Blaze.Html (Html)
+import Text.Blaze.Html.Renderer.String (renderHtml)
+import ClassyPrelude
 
 data BreadcrumbsItem =
     BreadcrumbsItem
@@ -9,5 +11,17 @@ data BreadcrumbsItem =
     , url :: Maybe Text -- The URL of the item. If Nothing, it will only show the label.
     , isActive :: Bool -- Determine if the breadcrumbs item should be marked as active.
     }
-    -- @todo: Can't due to label :: Html
-    -- deriving(Show)
+
+-- Html doesn't have a Show instance, so we define it manually and use 'renderHtml' for that.
+instance Show BreadcrumbsItem where
+    show breadcrumbsItem =
+        [plain|
+            { label = "#{label}"
+            , url = #{url}
+            , isActive = #{isActive}
+            }
+        |]
+        where
+            label = renderHtml $ get #label breadcrumbsItem
+            url = ClassyPrelude.show $ get #url breadcrumbsItem
+            isActive = ClassyPrelude.show $ get #isActive breadcrumbsItem
