9b4468f742439996b254d7f2bc654630735b0b44
Marc Scholten <marc@digitallyinduced.com>
2021-10-26 08:39:38 +0200

    Allow optimized nix-build's

--- NixSupport/default.nix
+++ NixSupport/default.nix
@@ -5,6 +5,7 @@
 , projectPath ? ./.
 , withHoogle ? false
 , additionalNixpkgsOptions ? {}
+, optimized ? false
 }:
 
 let
@@ -16,6 +17,14 @@ let
       else ghc.ghcWithPackages)
         (p: builtins.concatLists [ [p.haskell-language-server] (haskellDeps p) ] );
     allNativePackages = builtins.concatLists [ (otherDeps pkgs) [pkgs.postgresql pkgs.makeWrapper] (if pkgs.stdenv.isDarwin then [] else []) ];
+
+    appBinary = if optimized
+      then "build/bin/RunOptimizedProdServer"
+      else "build/bin/RunUnoptimizedProdServer";
+
+    jobsBinary = if optimized
+      then "build/bin/RunJobsOptimized"
+      else "build/bin/RunJobs";
 in
     pkgs.stdenv.mkDerivation {
         name = "app";
@@ -27,23 +36,23 @@ in
           ln -s "${ihp}/lib/IHP" build/ihp-lib
           ln -s "${ihp}/lib" IHP/lib # Avoid the Makefile calling 'which RunDevServer'
 
-          make -B build/bin/RunUnoptimizedProdServer
+          make ${appBinary}
 
           if find -type d -iwholename \*/Job|grep .; then
-            make build/bin/RunJobs;
+            make ${jobsBinary};
           fi;
         '';
         installPhase = ''
           mkdir -p "$out"
           mkdir -p $out/bin
 
-          mv build/bin/RunUnoptimizedProdServer $out/bin/RunUnoptimizedProdServer
+          mv ${appBinary} $out/bin/RunProdServerWithoutOptions
           INPUT_HASH="$((basename $out) | cut -d - -f 1)"
-          makeWrapper $out/bin/RunUnoptimizedProdServer $out/bin/RunProdServer --set-default IHP_ASSET_VERSION $INPUT_HASH --prefix PATH : ${pkgs.lib.makeBinPath (otherDeps pkgs)}
+          makeWrapper $out/bin/RunProdServerWithoutOptions $out/bin/RunProdServer --set-default IHP_ASSET_VERSION $INPUT_HASH --prefix PATH : ${pkgs.lib.makeBinPath (otherDeps pkgs)}
 
-          if [ -f build/bin/RunJobs ]; then
-            mv build/bin/RunJobs $out/bin/RunJobsWithoutDeps;
-            makeWrapper $out/bin/RunJobsWithoutDeps $out/bin/RunJobs --set-default IHP_ASSET_VERSION $INPUT_HASH --prefix PATH : ${pkgs.lib.makeBinPath (otherDeps pkgs)}
+          if [ -f ${jobsBinary} ]; then
+            mv ${jobsBinary} $out/bin/RunJobsWithoutOptions;
+            makeWrapper $out/bin/RunJobsWithoutOptions $out/bin/RunJobs --set-default IHP_ASSET_VERSION $INPUT_HASH --prefix PATH : ${pkgs.lib.makeBinPath (otherDeps pkgs)}
           fi;
 
           mkdir -p "$out/lib/build"
