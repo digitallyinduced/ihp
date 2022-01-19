43c5f48066e6d8be41fbc4f778f50b6133c09a53
Jannis Jorre <jannis@jorre.dev>
2022-01-19 08:30:13 +0100

    implement paginatedSqlQuery and paginatedSqlQueryWithOptions

--- IHP/Pagination/ControllerFunctions.hs
+++ IHP/Pagination/ControllerFunctions.hs
@@ -8,6 +8,8 @@ module IHP.Pagination.ControllerFunctions
     paginateWithOptions,
     filterList,
     defaultPaginationOptions,
+    paginatedSqlQuery,
+    paginatedSqlQueryWithOptions,
 ) where
 
 import IHP.Prelude
@@ -21,8 +23,10 @@ import IHP.QueryBuilder
     ( HasQueryBuilder, filterWhereILike, limit, offset )
 import IHP.Fetch (fetchCount)
 
-import IHP.ModelSupport (GetModelByTableName)
+import IHP.ModelSupport (GetModelByTableName, sqlQuery, sqlQueryScalar)
 
+import Database.PostgreSQL.Simple.ToField (toField, Action)
+import Database.PostgreSQL.Simple.Types (Query(Query))
 
 -- | Paginate a query, with the following default options:
 --
@@ -87,34 +91,26 @@ paginateWithOptions :: forall controller table queryBuilderProvider joinRegister
     Options
     -> queryBuilderProvider table
     -> IO (queryBuilderProvider table, Pagination)
-paginateWithOptions options query =
-    let
-        -- Page and page size shouldn't be lower than 1.
-        page = max 1 $ paramOrDefault @Int 1 "page"
-        -- We limit the page size to a maximum of 200, to prevent users from
-        -- passing in query params with a value that could overload the
-        -- database (e.g. maxItems=100000)
-        pageSize = min (max 1 $ paramOrDefault @Int (maxItems options) "maxItems") 200
-    in do
-        count <- query
-            |> fetchCount
+paginateWithOptions options query = do
+    count <- query
+        |> fetchCount
 
-        let pagination = Pagination
-                { currentPage = page
-                , totalItems = fromIntegral count
-                , pageSize = pageSize
-                , window = windowSize options
-                }
+    let pageSize = pageSize' options
+        pagination = Pagination
+            { currentPage = page
+            , totalItems = fromIntegral count
+            , pageSize = pageSize
+            , window = windowSize options
+            }
 
-        let results = query
+    let results = query
+            |> limit pageSize
+            |> offset (offset' pageSize page)
 
-                |> limit pageSize
-                |> offset ((page - 1) * pageSize)
-
-        pure
-            ( results
-            , pagination
-            )
+    pure
+        ( results
+        , pagination
+        )
 
 -- | Reading from the 'filter' query parameter, filters a query according to the string entered in the
 --   filter box by the user (if any), on a given text-based field. Will return any results containing the
@@ -155,3 +151,87 @@ defaultPaginationOptions =
         { maxItems = 50
         , windowSize = 5
         }
+
+-- | Runs a raw sql query and adds pagination to it.
+--
+-- By default, the pagination uses the following options:
+-- 1. Maximum items per page: 50. Each page will show at most 50 items.
+-- 2. Selector window size: 5. The selector will show the current page, and 5 pages before and after it,
+--    if they exist.
+--
+-- This function should be used inside your controller action. It will do three things:
+--
+--     1. Using the 'page' (current page number to display) and 'maxItems' (which overrides the set maximum
+--        items per page) request parameters, this applies the the needed limit and offset to display the
+--        correct page. For instance, page 3 with a maxItems of 50 would produce a limit of 50 and an offset
+--        of 100 to display results 100 through 150.
+--     2. Returns a 'Pagination' state which should be passed through to your view and then,
+--        in turn, 'renderPagination'.
+--     3. Actually run the query and return the result.
+--
+-- __Example:__
+--
+-- > (users, pagination) <- paginatedSqlQuery "SELECT id, firstname, lastname FROM users" ()
+--
+-- Take a look at "IHP.QueryBuilder" for a typesafe approach on building simple queries.
+--
+-- *AutoRefresh:* When using 'paginatedSqlQuery' with AutoRefresh, you need to use 'trackTableRead' to let AutoRefresh know that you have accessed a certain table. Otherwise AutoRefresh will not watch table of your custom sql query.
+paginatedSqlQuery
+  :: forall model
+   . ( FromRow model
+     , ?context :: ControllerContext
+     , ?modelContext :: ModelContext
+     )
+  => ByteString -> [Action] -> IO ([model], Pagination)
+paginatedSqlQuery = paginatedSqlQueryWithOptions defaultPaginationOptions
+
+-- | Runs a raw sql query and adds pagination to it.
+--
+-- This function accepts the same Options as 'paginateWithOptions', but otherwise behaves like 'paginatedSqlQuery'.
+--
+-- __Example:__
+--
+-- > (users, pagination) <- paginatedSqlQueryWithOptions
+-- >     (defaultPaginationOptions |> set #maxItems 10)
+-- >     "SELECT id, firstname, lastname FROM users"
+-- >     ()
+--
+-- Take a look at "IHP.QueryBuilder" for a typesafe approach on building simple queries.
+--
+-- *AutoRefresh:* When using 'paginatedSqlQuery' with AutoRefresh, you need to use 'trackTableRead' to let AutoRefresh know that you have accessed a certain table. Otherwise AutoRefresh will not watch table of your custom sql query.
+paginatedSqlQueryWithOptions
+  :: forall model
+   . ( FromRow model
+     , ?context :: ControllerContext
+     , ?modelContext :: ModelContext
+     )
+  => Options -> ByteString -> [Action] -> IO ([model], Pagination)
+paginatedSqlQueryWithOptions options sql placeholders = do
+    count :: Int <- sqlQueryScalar (Query $ "SELECT count(subquery.*) FROM (" <> sql <> ") as subquery") placeholders
+
+    let pageSize = pageSize' options
+        pagination = Pagination
+            { pageSize = pageSize
+            , totalItems = fromIntegral count
+            , currentPage = fromIntegral page
+            , window = windowSize options
+            }
+
+    results :: [model] <- sqlQuery
+        (Query $ "SELECT subquery.* FROM (" <> sql <> ") as subquery LIMIT ? OFFSET ?")
+        (placeholders ++ map toField [pageSize, offset' pageSize page])
+
+    pure (results, pagination)
+
+-- We limit the page size to a maximum of 200, to prevent users from
+-- passing in query params with a value that could overload the
+-- database (e.g. maxItems=100000)
+pageSize' :: (?context::ControllerContext) => Options -> Int
+pageSize' options = min (max 1 $ paramOrDefault @Int (maxItems options) "maxItems") 200
+
+-- Page and page size shouldn't be lower than 1.
+page :: (?context::ControllerContext) => Int
+page = max 1 $ paramOrDefault @Int 1 "page"
+
+offset' :: Int -> Int -> Int
+offset' pageSize page = (page - 1) * pageSize
