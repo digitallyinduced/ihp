e336a937faf1daf78a88de6126516061208bdaa3
Jannis Jorre <jannis@jorre.dev>
2022-03-09 10:06:27 +0100

    add compiler implementation for exclude constraints

--- Test/IDE/SchemaDesigner/CompilerSpec.hs
+++ Test/IDE/SchemaDesigner/CompilerSpec.hs
@@ -267,6 +267,55 @@ tests = do
                     }
             compileSql [statement] `shouldBe` "ALTER TABLE posts ADD CONSTRAINT check_title_length CHECK (length(title) >= 20);\n"
 
+        it "should compile ALTER TABLE .. ADD CONSTRAINT .. EXCLUDE .." do
+            let statement = AddConstraint
+                    { tableName = "posts"
+                    , constraint = ExcludeConstraint
+                        { name = "unique_title_by_author"
+                        , excludeElements =
+                            [ ExcludeConstraintElement { element = "title", operator = "=" }
+                            , ExcludeConstraintElement { element = "author", operator = "=" }
+                            ]
+                        , predicate = Nothing
+                        }
+                    }
+            compileSql [statement] `shouldBe` "ALTER TABLE posts ADD CONSTRAINT unique_title_by_author EXCLUDE (title WITH =, author WITH =);\n"
+
+        it "should compile ALTER TABLE .. ADD CONSTRAINT .. EXCLUDE .. WHERE .." do
+            let statement = AddConstraint
+                    { tableName = "posts"
+                    , constraint = ExcludeConstraint
+                        { name = "unique_title_by_author"
+                        , excludeElements =
+                            [ ExcludeConstraintElement { element = "title", operator = "=" }
+                            , ExcludeConstraintElement { element = "author", operator = "=" }
+                            ]
+                        , predicate = Just $ EqExpression (VarExpression "title") (TextExpression "why")
+                        }
+                    }
+            compileSql [statement] `shouldBe` "ALTER TABLE posts ADD CONSTRAINT unique_title_by_author EXCLUDE (title WITH =, author WITH =) WHERE (title = 'why');\n"
+
+        it "should compile ALTER TABLE .. ADD CONSTRAINT .. EXCLUDE .. WHERE .. with various operators" do
+            let statement = AddConstraint
+                    { tableName = "posts"
+                    , constraint = ExcludeConstraint
+                        { name = "unique_title_by_author"
+                        , excludeElements =
+                            [ ExcludeConstraintElement { element = "i1", operator = "=" }
+                            , ExcludeConstraintElement { element = "i2", operator = "<>" }
+                            , ExcludeConstraintElement { element = "i3", operator = "!=" }
+                            , ExcludeConstraintElement { element = "i4", operator = "<" }
+                            , ExcludeConstraintElement { element = "i5", operator = "<=" }
+                            , ExcludeConstraintElement { element = "i6", operator = ">" }
+                            , ExcludeConstraintElement { element = "i7", operator = ">=" }
+                            , ExcludeConstraintElement { element = "i8", operator = "AND" }
+                            , ExcludeConstraintElement { element = "i9", operator = "OR" }
+                            ]
+                        , predicate = Just $ EqExpression (VarExpression "title") (TextExpression "why")
+                        }
+                    }
+            compileSql [statement] `shouldBe` "ALTER TABLE posts ADD CONSTRAINT unique_title_by_author EXCLUDE (i1 WITH =, i2 WITH <>, i3 WITH !=, i4 WITH <, i5 WITH <=, i6 WITH >, i7 WITH >=, i8 WITH AND, i9 WITH OR) WHERE (title = 'why');\n"
+
         it "should compile a CREATE TABLE with text default value in columns" do
             let sql = cs [plain|CREATE TABLE a (\n    content TEXT DEFAULT 'example text' NOT NULL\n);\n|]
             let statement = StatementCreateTable CreateTable
