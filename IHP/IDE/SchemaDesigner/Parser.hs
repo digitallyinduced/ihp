e92a967d7495fb332d97d0f86da65920f55fea93
s0kil <dtechtours@gmail.com>
2022-03-11 22:26:08 -0500

    Help The Compiler

--- IHP/IDE/SchemaDesigner/Parser.hs
+++ IHP/IDE/SchemaDesigner/Parser.hs
@@ -569,15 +569,7 @@ createFunction = do
     orReplace <- isJust <$> optional (lexeme "OR" >> lexeme "REPLACE")
     lexeme "FUNCTION"
     functionName <- qualifiedIdentifier
-
-    let functionParameter = do
-        parameterName <- qualifiedIdentifier
-        space
-        parameterType <- qualifiedIdentifier
-        pure (parameterName, parameterType)
-
     functionParameters <- between (char '(') (char ')') (functionParameter `sepBy` (char ',' >> space))
-
     space
     lexeme "RETURNS"
     returns <- sqlType
@@ -598,6 +590,12 @@ createFunction = do
             symbol "plpgsql" <|> symbol "SQL"
     char ';'
     pure CreateFunction { functionName, functionParameters, functionBody, orReplace, returns, language }
+    where
+        functionParameter = do
+            parameterName <- qualifiedIdentifier
+            space
+            parameterType <- qualifiedIdentifier
+            pure (parameterName, parameterType)
 
 createTrigger = do
     lexeme "CREATE"
