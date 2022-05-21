eb5e0ecaf9f31f25ea604b5ef8beb7411df9082b
Marc Scholten <marc@digitallyinduced.com>
2022-05-21 22:04:04 +0200

    Splitted Lockable.isLocked into an IO and a pure function

--- IHP/AuthSupport/Lockable.hs
+++ IHP/AuthSupport/Lockable.hs
@@ -15,8 +15,11 @@ lockDuration = let timeInSecs = 60 * 60 in secondsToNominalDiffTime timeInSecs
 isLocked :: forall user. (HasField "lockedAt" user (Maybe UTCTime)) => user -> IO Bool
 isLocked user = do
     now <- getCurrentTime
-    let currentLockedAt :: Maybe UTCTime = getField @"lockedAt" user
-    pure $! case currentLockedAt of
+    pure (isLocked' now user)
+
+isLocked' :: forall user. (HasField "lockedAt" user (Maybe UTCTime)) => UTCTime -> user -> Bool
+isLocked' now user =
+    case getField @"lockedAt" user of
         Just lockedAt ->
             let diff = diffUTCTime now lockedAt
             in diff < lockDuration
