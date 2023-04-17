4c6cf60d7c2157fc7a0e8b23336f95530dc1184c
Marc Scholten <marc@digitallyinduced.com>
2023-04-17 23:21:56 +0200

    Added ihp-openai

---
+++ NixSupport/haskell-packages/ihp-openai.nix
@@ -0,0 +1,18 @@
+{ mkDerivation, lib, base
+, text, http-streams, retry, io-streams, bytestring, aeson, HsOpenSSL
+}:
+mkDerivation {
+  pname = "ihp-openai";
+  version = "1.0.1";
+  src = ./../../ihp-openai;
+  isLibrary = true;
+  isExecutable = false;
+  libraryHaskellDepends = [
+    base text http-streams retry io-streams bytestring aeson HsOpenSSL
+  ];
+  enableLibraryForGhci = true;
+  homepage = "https://ihp.digitallyinduced.com";
+  configureFlags = ["--enable-optimization=2"];
+  description = "Call GPT4 from IHP";
+  license = lib.licenses.mit;
+}
---
+++ ihp-openai/shell.nix
@@ -0,0 +1,22 @@
+let
+    pkgs = (import ./../NixSupport/make-nixpkgs-from-options.nix) {
+        ihp = ./../.;
+        haskellPackagesDir = ./../NixSupport/haskell-packages;
+    };
+    ghc = pkgs.haskell.packages.ghc944;
+    haskellDeps = ghc.ghcWithPackages (p: with p; [
+        text
+        bytestring
+        http-streams
+        retry
+
+        # Development Specific Tools
+        cabal-install
+    ]);
+in
+    pkgs.stdenv.mkDerivation {
+        name = "ihp-hsx";
+        src = ./.;
+        buildInputs = [haskellDeps];
+        shellHook = "eval $(egrep ^export ${haskellDeps}/bin/ghc)";
+    }
