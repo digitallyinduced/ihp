aefb85c72bee3243a1d53525d0f79c0875cd92d6
Marc Scholten <marc@digitallyinduced.com>
2023-05-01 20:27:22 +0200

    removed hash

--- NixSupport/haskell-packages/ihp-sentry.nix
+++ NixSupport/haskell-packages/ihp-sentry.nix
@@ -3,7 +3,6 @@
 mkDerivation {
   pname = "ihp-sentry";
   version = "0.0.1";
-  sha256 = "1ee92bece3c2bbf153dac013ee854fe8132702ee74cb61c07e7999ca1e35496d";
   libraryHaskellDepends = [
     base ihp raven-haskell wai warp
   ];
