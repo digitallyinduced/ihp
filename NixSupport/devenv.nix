68163c2fc73902ec92f1b627a56c39cdab1f3e67
Marc Scholten <marc@digitallyinduced.com>
2023-05-13 14:06:19 +0200

    Make devenv work better with existing IHP projects

--- NixSupport/devenv.nix
+++ NixSupport/devenv.nix
@@ -36,18 +36,39 @@ in
         Path to the IHP project.
       '';
     };
+
+    dontCheckPackages = lib.mkOption {
+      default = [];
+      description = ''
+        List of Haskell package names whose tests are skipped during build
+      '';
+    };
+    
+    doJailbreakPackages = lib.mkOption {
+      default = [];
+      description = ''
+        List of Haskell package names who are jailbreaked before build
+      '';
+    };
+    dontHaddockPackages = lib.mkOption {
+      default = [];
+      description = ''
+        List of Haskell package names whose haddock is not build during app build
+      '';
+    };
   };
 
   config =
     let
       ghcCompiler = import ./mkGhcCompiler.nix {
         inherit pkgs;
-        inherit (cfg) ghcCompiler;
+        inherit (cfg) ghcCompiler dontCheckPackages doJailbreakPackages dontHaddockPackages;
         ihp = ../.;
+        haskellPackagesDir = cfg.projectPath + "/Config/nix/haskell-packages";
       };
     in
     lib.mkIf cfg.enable {
-      packages = [ ghcCompiler.ihp ]; 
+      packages = [ ghcCompiler.ihp pkgs.postgresql_13 ]; 
 
       languages.haskell.enable = true;
       languages.haskell.package = ghcCompiler.ghc.withPackages cfg.haskellPackages;
@@ -58,10 +79,15 @@ in
 
       processes.devServer.exec = "start";
 
-      env.IHP_DEVENV = "1";
-      env.DATABASE_URL = "postgres:///app?host=${config.env.PGHOST}";
+      # Disabled for now
+      # Can be re-enabled once postgres is provided by devenv instead of IHP
+      # env.IHP_DEVENV = "1";
+      # env.DATABASE_URL = "postgres:///app?host=${config.env.PGHOST}";
 
-      services.postgres.enable = true;
+      # Disabled for now
+      # As the devenv postgres uses a different location for the socket
+      # this would break lots of known commands such as `make db`
+      services.postgres.enable = false;
       services.postgres.initialDatabases = [
         {
           name = "app";
