5334e5544d22b1989afc0b4948e62cd003082f6c
Marc Scholten <marc@digitallyinduced.com>
2023-06-05 15:01:28 +0200

    fixed script

--- .github/patch-flakes.php
+++ .github/patch-flakes.php
@@ -1,7 +1,6 @@
 <?php
 $file_contents = file_get_contents('./flake.nix');
-$new_content = preg_replace("/\".{40}\"/", "\"$argv[1]\"", $file_contents);
-$new_content_with_ref = preg_replace('/(ihp\.url = \")(.+)(\")/', '$1github:digitallyinduced/ihp?ref=${{ github.ref }}$3', $new_content);
+$new_content_with_ref = preg_replace('/(ihp\.url = \")(.+)(\")/', '$1github:digitallyinduced/ihp?ref=' . $argv[1] . '$3', $file_contents);
 $new_content_with_ref = str_replace("p.ihp", "p.ihp wreq mmark mmark-ext strip-ansi-escape stripe-signature stripe-concepts http-conduit haskell-to-elm aeson-casing tz tagsoup ihp-zip minio-hs hs-brotli wai-middleware-brotli fakedata jwt ihp-openai ihp-graphql", $new_content_with_ref);
 file_put_contents('./flake.nix', $new_content_with_ref);
 ?>
