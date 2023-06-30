8f2fa65ded91181c7e2bc42a6a43ced13a5be918
Paul d'Hubert <paul.dhubert@pm.me>
2023-06-30 20:42:44 +0200

    feat: do not watch .direnv and .devenv subdirectories

--- IHP/IDE/FileWatcher.hs
+++ IHP/IDE/FileWatcher.hs
@@ -3,6 +3,8 @@ module IHP.IDE.FileWatcher (withFileWatcher) where
 import IHP.Prelude
 import Control.Exception
 import Control.Concurrent (threadDelay, myThreadId)
+import Control.Monad (filterM)
+import System.Directory (listDirectory, doesDirectoryExist)
 import qualified System.FSNotify as FS
 import IHP.IDE.Types
 import qualified Data.Time.Clock as Clock
@@ -13,8 +15,26 @@ withFileWatcher :: (?context :: Context) => IO () -> IO ()
 withFileWatcher inner = withAsync callback \_ -> inner
     where
         callback = FS.withManagerConf fileWatcherConfig \manager -> do
-                FS.watchTree manager "." shouldActOnFileChange handleFileChange
+                watchRootDirectoryFiles manager
+                watchSubDirectories manager
                 forever (threadDelay maxBound) `finally` FS.stopManager manager
+        watchRootDirectoryFiles manager = 
+                FS.watchDir manager "." shouldActOnFileChange handleFileChange 
+        watchSubDirectories manager = do
+                directories <- listWatchableDirectories
+                forM_ directories \directory -> do
+                  FS.watchTree manager directory shouldActOnFileChange handleFileChange
+           
+listWatchableDirectories :: IO [String]
+listWatchableDirectories = do
+  rootDirectoryContents <- listDirectory "."
+  filterM shouldWatchDirectory rootDirectoryContents
+
+shouldWatchDirectory :: String -> IO Bool
+shouldWatchDirectory path = do
+  isDirectory <- doesDirectoryExist path
+  pure $ isDirectory && path /= ".devenv" && path /= ".direnv"
+
 
 fileWatcherDebounceTime :: NominalDiffTime
 fileWatcherDebounceTime = Clock.secondsToNominalDiffTime 0.1 -- 100ms
