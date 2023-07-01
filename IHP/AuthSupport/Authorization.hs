4dd08eddb56239abbee6fa39e208ea1aa9b1ee1b
Amitai Burstein <amitai@gizra.com>
2023-07-01 17:49:14 +0300

    Try to import

--- IHP/AuthSupport/Authorization.hs
+++ IHP/AuthSupport/Authorization.hs
@@ -5,7 +5,7 @@ Copyright: (c) digitally induced GmbH, 2020
 -}
 module IHP.AuthSupport.Authorization where
 
-import IHP.Prelude
+import IHP.Controller.Render
 
 class CanView user model where
     canView :: (?modelContext :: ModelContext) => model -> user -> IO Bool
@@ -13,13 +13,13 @@ class CanView user model where
 -- | Stops the action execution with an error message when the access condition is false.
 --
 -- __Example:__ Checking a user is author of a blog post.
--- 
+--
 -- > action EditPostAction { postId } = do
 -- >     post <- fetch postId
 -- >     accessDeniedUnless (post.authorId == currentUserId)
--- >     
+-- >
 -- >     renderHtml EditView { .. }
 --
 -- This will throw an error and prevent the view from being rendered when the current user is not author of the post.
 accessDeniedUnless :: Bool -> IO ()
-accessDeniedUnless condition = if condition then pure () else fail "Access denied"
+accessDeniedUnless condition = unless condition (fail "Access denied")
