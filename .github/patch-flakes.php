4e6df9a26181d8a848ec4aa7fbcbf820aec68eee
Marc Scholten <marc@digitallyinduced.com>
2023-07-14 21:49:36 +0200

    Added unordered-containers and minimorph to build

--- .github/patch-flakes.php
+++ .github/patch-flakes.php
@@ -1,7 +1,7 @@
 <?php
 $file_contents = file_get_contents('./flake.nix');
 $new_content_with_ref = preg_replace('/(ihp\.url = \")(.+)(\")/', '$1github:digitallyinduced/ihp?ref=' . $argv[1] . '$3', $file_contents);
-$new_content_with_ref = str_replace("p.ihp", "p.ihp wreq mmark mmark-ext strip-ansi-escape stripe-signature stripe-concepts http-conduit haskell-to-elm aeson-casing tz tagsoup ihp-zip minio-hs hs-brotli wai-middleware-brotli fakedata jwt ihp-openai ihp-graphql", $new_content_with_ref);
+$new_content_with_ref = str_replace("p.ihp", "p.ihp wreq mmark mmark-ext strip-ansi-escape stripe-signature stripe-concepts http-conduit haskell-to-elm aeson-casing tz tagsoup ihp-zip minio-hs hs-brotli wai-middleware-brotli fakedata jwt ihp-openai ihp-graphql unordered-containers minimorph", $new_content_with_ref);
 $new_content_with_ref = str_replace("# Native dependencies, e.g. imagemagick", "nodejs imagemagick elmPackages.elm", $new_content_with_ref);
 file_put_contents('./flake.nix', $new_content_with_ref);
 ?>
