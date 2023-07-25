705c24d5b26834e8551f1c4ed4724a4a0d3c59c0
Marc Scholten <marc@digitallyinduced.com>
2023-07-25 23:02:44 +0200

    fixed typerep-map

--- NixSupport/mkGhcCompiler.nix
+++ NixSupport/mkGhcCompiler.nix
@@ -35,7 +35,7 @@ let
     builtins.listToAttrs (map toPackage names);
 
   ihpDontCheckPackages = [ "mmark" "mmark-ext" "mutable-containers" "hiedb" "hls-fourmolu-plugin" ];
-  ihpDoJailbreakPackages = [ "haskell-to-elm" "ip" "ghc-syntax-highlighter" "relude" "hs-brotli" "tuples" "singletons-th" "singletons-base" "inflections" "postgresql-simple" "with-utf8" "chell" "zigzag" ];
+  ihpDoJailbreakPackages = [ "haskell-to-elm" "ip" "ghc-syntax-highlighter" "relude" "hs-brotli" "tuples" "singletons-th" "singletons-base" "inflections" "postgresql-simple" "with-utf8" "chell" "zigzag" "typerep-map" ];
   ihpDontHaddockPackages = [ ];
 in ghcCompiler.override {
   overrides = composeExtensionsList [
