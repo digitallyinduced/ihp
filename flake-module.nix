5755990f3736e3a432b1a1de4ab5fabd7e5569cf
Marc Scholten <marc@digitallyinduced.com>
2023-08-26 21:08:02 -0700

    Avoid nix shell call in deploy-to-nixos

--- flake-module.nix
+++ flake-module.nix
@@ -205,10 +205,14 @@ ihpFlake:
                 env.IHP = "${ihp}/lib/IHP"; # Used in the Makefile
 
                 scripts.deploy-to-nixos.exec = ''
-                    echo "usage: deploy-to-nixos <flake> <target-host>"
-                    echo "example: deploy-to-nixos stagging.example.com"
-                    nix shell nixpkgs#nixos-rebuild --command bash -c "nixos-rebuild switch -j auto --use-substitutes --fast --flake .#$1 --target-host $1 --build-host $1 --option substituters https://digitallyinduced.cachix.org --option trusted-public-keys digitallyinduced.cachix.org:digitallyinduced.cachix.org-1:y+wQvrnxQ+PdEsCt91rmvv39qRCYzEgGQaldK26hCKE="
-                    # ssh ihp-deploy-test systemctl start migrate
+                    if [[ $# -eq 0 || $1 == "--help" ]]; then
+                        echo "usage: deploy-to-nixos <target-host>"
+                        echo "example: deploy-to-nixos staging.example.com"
+                        exit 0
+                    fi
+
+                    ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch -j auto --use-substitutes --fast --flake .#$1 --target-host $1 --build-host $1 --option substituters https://digitallyinduced.cachix.org --option trusted-public-keys digitallyinduced.cachix.org:digitallyinduced.cachix.org-1:y+wQvrnxQ+PdEsCt91rmvv39qRCYzEgGQaldK26hCKE=
+                    ssh $1 systemctl start migrate
                 '';
             };
         };
