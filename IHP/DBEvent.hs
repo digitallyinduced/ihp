81bdc7c84985782732922aaafd9b09a36032b33f
kodeFant <lars.lillo@gmail.com>
2023-09-01 21:45:29 +0200

    Document code

--- IHP/DBEvent.hs
+++ IHP/DBEvent.hs
@@ -1,10 +1,9 @@
+-- | The IHP.DBEvent module is responsible for dispatching Server-Sent Events (SSE) with PostgreSQL notifications.
 module IHP.DBEvent (respondDbEvent, initDbEvents) where
 
 
 import IHP.Prelude
 import IHP.ControllerSupport
--- import qualified Network.Wai.Internal as Wai
--- import qualified Data.Binary.Builder as ByteString
 import qualified Data.Set as Set
 import IHP.ModelSupport ( withTableReadTracker, withRowLevelSecurityDisabled, sqlExec, trackTableRead )
 import qualified IHP.PGListener as PGListener
@@ -24,30 +23,32 @@ import Control.Concurrent (threadDelay, forkIO)
 import Database.PostgreSQL.Simple.Notification (notificationPid)
 
 
-
+-- | Initialize database events functionality by placing the PostgreSQL listener in the application context.
 initDbEvents :: (?context :: ControllerContext, ?applicationContext :: ApplicationContext) => IO ()
 initDbEvents = do
     putContext ?applicationContext.pgListener
 
 
-
+-- | Stream database change events to clients as Server-Sent Events.
 respondDbEvent :: (?modelContext :: ModelContext, ?context :: ControllerContext, ?touchedTables::IORef (Set ByteString)) => ByteString -> IO ()
 respondDbEvent eventName  = do
     touchedTables <- Set.toList <$> readIORef ?touchedTables
     pgListener <- fromContext @PGListener.PGListener
 
     let streamBody sendChunk flush = do
+             -- Notify the client that the connection is established
             sendChunk (ByteString.stringUtf8 "data: Connection established!\n\n") >> flush
-
+            -- For each touched table, create a trigger in the database and subscribe to notifications
             touchedTables 
                 |> mapM (\table -> do
                     let createTriggerSql = notificationTrigger table
 
+                    -- Disable row-level security while creating the trigger
                     withRowLevelSecurityDisabled do
                         sqlExec createTriggerSql ()
                         pure ()
 
-                    
+                    -- Subscribe to the table's change notifications
                     pgListener |> PGListener.subscribe (channelName table) \notification -> do
                         let pid = notification.notificationPid |> show |> cs
                         sendChunk (ByteString.stringUtf8 $
@@ -60,11 +61,12 @@ respondDbEvent eventName  = do
                                 `Exception.catch` (\e -> putStrLn $ "Error sending chunk: " ++ show (e :: Exception.SomeException)
                             )
                         )
-
+            -- Send a heartbeat to the client every 30 seconds to keep the connection alive
             forever do
                 threadDelay (30 * 1000000)
                 sendChunk (ByteString.stringUtf8 $ ": heartbeat\n\n") >> flush `Exception.catch` (\e -> putStrLn $ "Error sending heartbeat: " ++ show (e :: Exception.SomeException))
 
+    -- Send the stream to the client
     respondAndExit $
         responseStream
             status200
@@ -75,10 +77,11 @@ respondDbEvent eventName  = do
             streamBody
 
 
-
+-- | Generate the channel name for PostgreSQL notifications based on the table name.
 channelName :: ByteString -> ByteString
 channelName tableName = "dbe_did_change_" <> tableName
 
+-- | Construct the SQL for creating triggers on table changes and sending notifications to the corresponding channel.
 notificationTrigger :: ByteString -> PG.Query
 notificationTrigger tableName = PG.Query [i|
         BEGIN;
