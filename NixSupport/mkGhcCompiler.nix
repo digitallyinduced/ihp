fb5c5e3b55198c28515a21d74603ce05ef0ecdb1
Marc Scholten <marc@digitallyinduced.com>
2023-09-02 13:33:36 -0700

    Update mkGhcCompiler.nix

--- NixSupport/mkGhcCompiler.nix
+++ NixSupport/mkGhcCompiler.nix
@@ -36,7 +36,7 @@ let
     builtins.listToAttrs (map toPackage names);
 
   ihpDontCheckPackages = [ "mmark" "mmark-ext" "mutable-containers" "hiedb" "hls-fourmolu-plugin" ];
-  ihpDoJailbreakPackages = [ "haskell-to-elm" "ip" "ghc-syntax-highlighter" "relude" "hs-brotli" "tuples" "singletons-th" "singletons-base" "inflections" "postgresql-simple" "with-utf8" "chell" ];
+  ihpDoJailbreakPackages = [ "haskell-to-elm" "ip" "ghc-syntax-highlighter" "relude" "hs-brotli" "tuples" "singletons-th" "singletons-base" "inflections" "postgresql-simple" "with-utf8" "chell" "typerep-map"];
   ihpDontHaddockPackages = [ ];
 in ghcCompiler.override {
   overrides = composeExtensionsList [
