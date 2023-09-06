31105704df83edd1da10a12cef80504e8722fc98
Marc Scholten <marc@digitallyinduced.com>
2023-09-06 13:30:30 -0700

    Disable haskell-to-elm

--- .github/patch-flakes.php
+++ .github/patch-flakes.php
@@ -2,7 +2,7 @@
 $file_contents = file_get_contents('./flake.nix');
 $new_content_with_ref = preg_replace('/(ihp\.url = \")(.+)(\")/', '$1' . 'tarball+' . $argv[1] . '$3', $file_contents);
 $proPackages = "ihp-sentry ihp-oauth-google ihp-oauth-github ihp-stripe";
-$new_content_with_ref = str_replace("p.ihp", "p.ihp wreq mmark mmark-ext strip-ansi-escape stripe-signature stripe-concepts http-conduit haskell-to-elm aeson-casing tz tagsoup ihp-zip minio-hs hs-brotli wai-middleware-brotli fakedata jwt ihp-openai ihp-graphql unordered-containers minimorph $proPackages", $new_content_with_ref);
+$new_content_with_ref = str_replace("p.ihp", "p.ihp wreq mmark mmark-ext strip-ansi-escape stripe-signature stripe-concepts http-conduit aeson-casing tz tagsoup ihp-zip minio-hs hs-brotli wai-middleware-brotli fakedata jwt ihp-openai ihp-graphql unordered-containers minimorph $proPackages", $new_content_with_ref);
 $new_content_with_ref = str_replace("# Native dependencies, e.g. imagemagick", "nodejs imagemagick elmPackages.elm", $new_content_with_ref);
 file_put_contents('./flake.nix', $new_content_with_ref);
 ?>
