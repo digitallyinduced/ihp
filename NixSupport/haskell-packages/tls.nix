b29aca47d2d45acef15cca32efeb0c8ad88c9935
Marc Scholten <marc@digitallyinduced.com>
2023-09-08 10:49:39 -0700

    Try a different tls version

--- NixSupport/haskell-packages/tls.nix
+++ NixSupport/haskell-packages/tls.nix
@@ -6,8 +6,8 @@
 }:
 mkDerivation {
   pname = "tls";
-  version = "1.9.0";
-  sha256 = "0gj3af9sqd0bw7dpcqfyvbslpxsk2ij00a77kl710fwhw35vj1an";
+  version = "1.7.1";
+  sha256 = "0q277cp2dqp5cs7mmzqavi8sm0km7xbf2zgcqjp17491w7q5a687";
   libraryHaskellDepends = [
     asn1-encoding asn1-types async base bytestring cereal crypton
     crypton-x509 crypton-x509-store crypton-x509-validation
@@ -23,7 +23,7 @@ mkDerivation {
     crypton-x509-validation data-default-class gauge hourglass
     QuickCheck tasty-quickcheck
   ];
-  homepage = "https://github.com/haskell-tls/hs-tls";
+  homepage = "http://github.com/vincenthz/hs-tls";
   description = "TLS/SSL protocol native implementation (Server and Client)";
   license = lib.licenses.bsd3;
 }
