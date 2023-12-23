abe950a841dbdf7f7afb3a3d62fd2276dc109167
Amitai Burstein <amitai@gizra.com>
2023-12-23 16:43:55 +0200

    Try to add a test

--- Test/View/FormSpec.hs
+++ Test/View/FormSpec.hs
@@ -29,7 +29,7 @@ tests = do
                     {submitButton}
                 |]
                 form `shouldRenderTo` "<form method=\"POST\" action=\"/CreateProject\" id=\"\" class=\"new-form\" data-disable-javascript-submission=\"false\"><div class=\"mb-3\" id=\"form-group-project_title\"><label class=\"form-label\" for=\"project_title\">Title</label><input type=\"text\" name=\"title\" placeholder=\"\" id=\"project_title\" class=\"form-control\"> </div> <button class=\"btn btn-primary\" type=\"submit\">Create Project</button></form>"
-            
+
             it "should render a form with a GET method" do
                 context <- createControllerContext
                 let ?context = context
@@ -41,6 +41,19 @@ tests = do
                     {submitButton}
                 |]
                 form `shouldRenderTo` "<form method=\"GET\" action=\"/CreateProject\" id=\"\" class=\"new-form\" data-disable-javascript-submission=\"false\"><div class=\"mb-3\" id=\"form-group-project_title\"><label class=\"form-label\" for=\"project_title\">Title</label><input type=\"text\" name=\"title\" placeholder=\"\" id=\"project_title\" class=\"form-control\"> </div> <button class=\"btn btn-primary\" type=\"submit\">Create Project</button></form>"
+
+            it "should render a form with disabled button" do
+                context <- createControllerContext
+                let ?context = context
+
+                let options formContext = formContext |> set #formMethod "GET"
+
+                let form = formForWithOptions project options [hsx|
+                    {textField #title}
+                    {submitButton {disabled = True}}
+                |]
+                form `shouldRenderTo` "<form method=\"GET\" action=\"/CreateProject\" id=\"\" class=\"new-form\" data-disable-javascript-submission=\"false\"><div class=\"mb-3\" id=\"form-group-project_title\"><label class=\"form-label\" for=\"project_title\">Title</label><input type=\"text\" name=\"title\" placeholder=\"\" id=\"project_title\" class=\"form-control\"> </div> <button class=\"btn btn-primary\" disabled=\"disabled\" type=\"submit\">Create Project</button></form>"
+
 
 shouldRenderTo renderFunction expectedHtml = Blaze.renderMarkup renderFunction `shouldBe` expectedHtml
 
@@ -54,7 +67,7 @@ createControllerContext = do
 
 data Project'  = Project {id :: (Id' "projects"), title :: Text, meta :: MetaBag} deriving (Eq, Show)
 instance InputValue Project where inputValue = IHP.ModelSupport.recordToInputValue
-type Project = Project' 
+type Project = Project'
 
 type instance GetTableName (Project' ) = "projects"
 type instance GetModelByTableName "projects" = Project
