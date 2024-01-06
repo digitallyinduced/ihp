edf2bf6b3684982dbf9b50ce63464278773d3e38
Marc Scholten <marc@digitallyinduced.com>
2024-01-06 18:00:39 +0100

    Extracted out duplicate binary cache config

--- NixSupport/nixosModules/appWithPostgres.nix
+++ NixSupport/nixosModules/appWithPostgres.nix
@@ -1,18 +1,18 @@
 # Running IHP app + a local Postgres connected to it
-{ config, pkgs, modulesPath, lib, ihp, ... }:
+{ config, nixpkgs, pkgs, modulesPath, lib, ihp, ... }:
 let cfg = config.services.ihp;
 in
 {
     imports = [
         ihp.nixosModules.options
+        ihp.nixosModules.binaryCache
         ihp.nixosModules.services_app
         ihp.nixosModules.services_worker
         ihp.nixosModules.services_migrate
     ];
 
-    # Speed up builds with the IHP binary cache
-    nix.settings.substituters = [ "https://digitallyinduced.cachix.org" ];
-    nix.settings.trusted-public-keys = [ "digitallyinduced.cachix.org-1:y+wQvrnxQ+PdEsCt91rmvv39qRCYzEgGQaldK26hCKE=" ];
+    # Pin the nixpkgs to the IHP nixpkgs
+    nix.registry.nixpkgs.flake = nixpkgs;
     
     # Add swap to avoid running out of memory during builds
     swapDevices = [ { device = "/swapfile"; size = 8192; } ];
