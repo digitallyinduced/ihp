3e968717bdebb680565d923ec046c48e1c273243
Mona Mayrhofer <mona.mayrhofer@proton.me>
2024-03-02 12:06:46 +0100

    add several unit for arbitrary pks

--- Test/SchemaCompilerSpec.hs
+++ Test/SchemaCompilerSpec.hs
@@ -442,6 +442,120 @@ tests = do
                             builder |> QueryBuilder.filterWhere (#id, id)
                         {-# INLINE filterWhereId #-}
                 |]
+        describe "compileStatementPreview for table with arbitrarily named primary key" do
+            let statements = parseSqlStatements [trimming|
+                CREATE TABLE things (
+                    thing_arbitrary_ident UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+                );
+                CREATE TABLE others (
+                    other_arbitrary_ident UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+                    thing_ref UUID NOT NULL
+                );
+                ALTER TABLE others ADD CONSTRAINT other_thing_refs FOREIGN KEY (thing_ref) REFERENCES things (thing_arbitrary_ident) ON DELETE NO ACTION;
+            |]
+            let
+                isTargetTable :: Statement -> Bool
+                isTargetTable (StatementCreateTable CreateTable { name }) = name == "things"
+                isTargetTable otherwise = False
+            let (Just statement) = find isTargetTable statements
+            let compileOutput = compileStatementPreview statements statement |> Text.strip
+        
+            it "should compile CanCreate instance with sqlQuery" $ \statement -> do
+                getInstanceDecl "CanCreate" compileOutput `shouldBe` [trimming|
+                    instance CanCreate Thing where
+                        create :: (?modelContext :: ModelContext) => Thing -> IO Thing
+                        create model = do
+                            List.head <$> sqlQuery "INSERT INTO things (thing_arbitrary_ident) VALUES (?) RETURNING thing_arbitrary_ident" (Only (fieldWithDefault #thingArbitraryIdent model))
+                        createMany [] = pure []
+                        createMany models = do
+                            sqlQuery (Query $ "INSERT INTO things (thing_arbitrary_ident) VALUES " <> (ByteString.intercalate ", " (List.map (\_ -> "(?)") models)) <> " RETURNING thing_arbitrary_ident") (List.concat $ List.map (\model -> [toField (fieldWithDefault #thingArbitraryIdent model)]) models)
+                    |]
+            it "should compile CanUpdate instance with sqlQuery" $ \statement -> do
+                getInstanceDecl "CanUpdate" compileOutput `shouldBe` [trimming|
+                    instance CanUpdate Thing where
+                        updateRecord model = do
+                            List.head <$> sqlQuery "UPDATE things SET thing_arbitrary_ident = ? WHERE thing_arbitrary_ident = ? RETURNING thing_arbitrary_ident" ((fieldWithUpdate #thingArbitraryIdent model, model.thingArbitraryIdent))
+                    |]
+            it "should compile FromRow instance" $ \statement -> do
+                getInstanceDecl "FromRow" compileOutput `shouldBe` [trimming|
+                    instance FromRow Thing where
+                        fromRow = do
+                            thingArbitraryIdent <- field
+                            let theRecord = Thing thingArbitraryIdent (QueryBuilder.filterWhere (#thingRef, thingArbitraryIdent) (QueryBuilder.query @Other)) def { originalDatabaseRecord = Just (Data.Dynamic.toDyn theRecord) }
+                            pure theRecord
+                    |]
+            it "should compile QueryBuilder.FilterPrimaryKey instance" $ \statement -> do
+                getInstanceDecl "QueryBuilder.FilterPrimaryKey" compileOutput `shouldBe` [trimming|
+                    instance QueryBuilder.FilterPrimaryKey "things" where
+                        filterWhereId thingArbitraryIdent builder =
+                            builder |> QueryBuilder.filterWhere (#thingArbitraryIdent, thingArbitraryIdent)
+                        {-# INLINE filterWhereId #-}
+                    |]
+        describe "compileStatementPreview for table with composite primary key" do
+            let statements = parseSqlStatements [trimming|
+                CREATE TABLE bits (
+                    bit_arbitrary_ident UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+                );
+                CREATE TABLE parts (
+                    part_arbitrary_ident UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+                );
+                CREATE TABLE bit_part_refs (
+                    bit_ref UUID NOT NULL,
+                    part_ref UUID NOT NULL,
+                    PRIMARY KEY(bit_ref, part_ref)
+                );
+                ALTER TABLE bit_part_refs ADD CONSTRAINT bit_part_bit_refs FOREIGN KEY (bit_ref) REFERENCES bits (bit_arbitrary_ident) ON DELETE NO ACTION;
+                ALTER TABLE bit_part_refs ADD CONSTRAINT bit_part_part_refs FOREIGN KEY (part_ref) REFERENCES parts (part_arbitrary_ident) ON DELETE NO ACTION;
+            |]
+            let
+                isNamedTable :: Text -> Statement -> Bool
+                isNamedTable targetName (StatementCreateTable CreateTable { name }) = name == targetName
+                isNamedTable _ _ = False
+            let (Just statement) = find (isNamedTable "bit_part_refs") statements
+            let compileOutput = compileStatementPreview statements statement |> Text.strip
+        
+            it "should compile CanCreate instance with sqlQuery" $ \statement -> do
+                getInstanceDecl "CanCreate" compileOutput `shouldBe` [trimming|
+                    instance CanCreate BitPartRef where
+                        create :: (?modelContext :: ModelContext) => BitPartRef -> IO BitPartRef
+                        create model = do
+                            List.head <$> sqlQuery "INSERT INTO bit_part_refs (bit_ref, part_ref) VALUES (?, ?) RETURNING bit_ref, part_ref" ((model.bitRef, model.partRef))
+                        createMany [] = pure []
+                        createMany models = do
+                            sqlQuery (Query $ "INSERT INTO bit_part_refs (bit_ref, part_ref) VALUES " <> (ByteString.intercalate ", " (List.map (\_ -> "(?, ?)") models)) <> " RETURNING bit_ref, part_ref") (List.concat $ List.map (\model -> [toField (model.bitRef), toField (model.partRef)]) models)
+                    |]
+            it "should compile CanUpdate instance with sqlQuery" $ \statement -> do
+                getInstanceDecl "CanUpdate" compileOutput `shouldBe` [trimming|
+                    instance CanUpdate BitPartRef where
+                        updateRecord model = do
+                            List.head <$> sqlQuery "UPDATE bit_part_refs SET bit_ref = ?, part_ref = ? WHERE (bit_ref, part_ref) = (?, ?) RETURNING bit_ref, part_ref" ((fieldWithUpdate #bitRef model, fieldWithUpdate #partRef model, model.bitRef, model.partRef))
+                    |]
+            it "should compile FromRow instance" $ \statement -> do
+                getInstanceDecl "FromRow" compileOutput `shouldBe` [trimming|
+                    instance FromRow BitPartRef where
+                        fromRow = do
+                            bitRef <- field
+                            partRef <- field
+                            let theRecord = BitPartRef bitRef partRef def { originalDatabaseRecord = Just (Data.Dynamic.toDyn theRecord) }
+                            pure theRecord
+                    |]
+            it "should compile FromRow instance of table that references part of a composite key" $ \statement -> do
+                let (Just statement) = find (isNamedTable "parts") statements
+                let compileOutput = compileStatementPreview statements statement |> Text.strip
+                getInstanceDecl "FromRow" compileOutput `shouldBe` [trimming|
+                    instance FromRow Part where
+                        fromRow = do
+                            partArbitraryIdent <- field
+                            let theRecord = Part partArbitraryIdent (QueryBuilder.filterWhere (#partRef, partArbitraryIdent) (QueryBuilder.query @BitPartRef)) def { originalDatabaseRecord = Just (Data.Dynamic.toDyn theRecord) }
+                            pure theRecord
+                    |]
+            it "should compile QueryBuilder.FilterPrimaryKey instance" $ \statement -> do
+                getInstanceDecl "QueryBuilder.FilterPrimaryKey" compileOutput `shouldBe` [trimming|
+                    instance QueryBuilder.FilterPrimaryKey "bit_part_refs" where
+                        filterWhereId (Id (bitRef, partRef)) builder =
+                            builder |> QueryBuilder.filterWhere (#bitRef, bitRef) |> QueryBuilder.filterWhere (#partRef, partRef)
+                        {-# INLINE filterWhereId #-}
+                    |]
         describe "compileFilterPrimaryKeyInstance" do
             it "should compile FilterPrimaryKey instance when primary key is called id" do
                 let statement = StatementCreateTable $ CreateTable {
@@ -457,38 +571,6 @@ tests = do
                     instance QueryBuilder.FilterPrimaryKey "things" where
                         filterWhereId id builder =
                             builder |> QueryBuilder.filterWhere (#id, id)
-                        {-# INLINE filterWhereId #-}
-                    |]
-            it "should compile FilterPrimaryKey instance when primary key is called thing_id" do
-                let statement = StatementCreateTable $ CreateTable {
-                        name = "things",
-                        columns = [ Column "thing_id" PUUID Nothing False False Nothing ],
-                        primaryKeyConstraint = PrimaryKeyConstraint ["thing_id"],
-                        constraints = [],
-                        unlogged = False
-                    }
-                let compileOutput = compileStatementPreview [statement] statement |> Text.strip
-                
-                getInstanceDecl "QueryBuilder.FilterPrimaryKey" compileOutput `shouldBe` [trimming|
-                    instance QueryBuilder.FilterPrimaryKey "things" where
-                        filterWhereId thingId builder =
-                            builder |> QueryBuilder.filterWhere (#thingId, thingId)
-                        {-# INLINE filterWhereId #-}
-                    |]
-            it "should compile FilterPrimaryKey instance when primary key is composite of thing_id other_id" do
-                let statement = StatementCreateTable $ CreateTable {
-                        name = "thing_other_rels",
-                        columns = [ Column "thing_id" PUUID Nothing False False Nothing, Column "other_id" PUUID Nothing False False Nothing],
-                        primaryKeyConstraint = PrimaryKeyConstraint ["thing_id", "other_id"],
-                        constraints = [],
-                        unlogged = False
-                    }
-                let compileOutput = compileStatementPreview [statement] statement |> Text.strip
-                
-                getInstanceDecl "QueryBuilder.FilterPrimaryKey" compileOutput `shouldBe` [trimming|
-                    instance QueryBuilder.FilterPrimaryKey "thing_other_rels" where
-                        filterWhereId (Id (thingId, otherId)) builder =
-                            builder |> QueryBuilder.filterWhere (#thingId, thingId) |> QueryBuilder.filterWhere (#otherId, otherId)
                         {-# INLINE filterWhereId #-}
                     |]
 
