d5865c4cdd3fcc130cbbb0efc53cceda793a4dce
Amitai Burstein <amitai@gizra.com>
2024-10-05 20:31:50 +0300

    Change slash logic

--- IHP/FileStorage/ControllerFunctions.hs
+++ IHP/FileStorage/ControllerFunctions.hs
@@ -112,7 +112,7 @@ storeFileWithOptions fileInfo options = do
                 |> LBS.writeFile (cs destPath)
 
             let frameworkConfig = ?context.frameworkConfig
-            pure $ "/" <> objectPath
+            pure $ objectPath
         S3Storage { connectInfo, bucket, baseUrl } -> do
             let payload = fileInfo
                     |> (.fileContent)
@@ -230,9 +230,9 @@ createTemporaryDownloadUrlFromPathWithExpiredAt validInSeconds objectPath = do
 
             let url = if any (`isPrefixOf` objectPath) urlSchemes
                     -- BC, before we saved only the relative path of a file, we saved the full URL. So use it as is.
-                    then objectPath
+                    then "/" <> objectPath
                     -- We have the relative path, so add the baseUrl.
-                    else frameworkConfig.baseUrl <> objectPath
+                    else frameworkConfig.baseUrl <> "/" <> objectPath
 
             pure TemporaryDownloadUrl { url = cs url, expiredAt = publicUrlExpiredAt }
         S3Storage { connectInfo, bucket} -> do
