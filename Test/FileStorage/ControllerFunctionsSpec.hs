2aa73c0af90ddb428cc32a24d053e59c00fee0c8
Amitai Burstein <amitai@gizra.com>
2024-10-06 07:59:42 +0300

    More fixes

--- Test/FileStorage/ControllerFunctionsSpec.hs
+++ Test/FileStorage/ControllerFunctionsSpec.hs
@@ -34,17 +34,19 @@ tests = describe "IHP.FileStorage.ControllerFunctions" $ do
 
                 result.url `shouldBe` "Test.FileStorage.ControllerFunctionsSpec/test.txt"
 
-    -- describe "createTemporaryDownloadUrlFromPathWithExpiredAt" $ do
-    --     it "returns baseUrl concatenated with objectPath when objectPath does not start with http:// or https://" $ do
-    --         context <- newControllerContext
-    --         let objectPath = "static/test.txt"
-    --         temporaryDownloadUrl <- createTemporaryDownloadUrlFromPathWithExpiredAt 3600 objectPath
+    describe "createTemporaryDownloadUrlFromPathWithExpiredAt" $ do
+        it "returns baseUrl concatenated with objectPath when objectPath does not start with http:// or https://" $ do
+            context <- newControllerContext
+            let ?context = context
+            let objectPath = "static/test.txt"
+            temporaryDownloadUrl <- createTemporaryDownloadUrlFromPath objectPath
 
-    --         temporaryDownloadUrl.url `shouldBe` "http://localhost:8000/static/test.txt"
+            temporaryDownloadUrl.url `shouldBe` "http://localhost:8000/static/test.txt"
 
-    --     it "returns '/' concatenated with objectPath when objectPath starts with 'http://' or 'https://'" $ do
-    --         context <- newControllerContext
-    --         let objectPath = "https://example.com/static/test.txt"
-    --         temporaryDownloadUrl <- createTemporaryDownloadUrlFromPathWithExpiredAt 3600 objectPath
+        it "returns '/' concatenated with objectPath when objectPath starts with 'http://' or 'https://'" $ do
+            context <- newControllerContext
+            let ?context = context
+            let objectPath = "https://example.com/static/test.txt"
+            temporaryDownloadUrl <- createTemporaryDownloadUrlFromPath objectPath
 
-    --         temporaryDownloadUrl.url `shouldBe` "https://example.com/static/test.txt"
+            temporaryDownloadUrl.url `shouldBe` "https://example.com/static/test.txt"
