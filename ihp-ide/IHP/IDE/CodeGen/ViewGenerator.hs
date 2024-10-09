84f2cbdda9fb23a5c15c0d108f318cfa66733413
Liam McDermott <liam@woolwichweb.works>
2024-10-09 17:03:57 -0400

    Fix view generator tests.
    - Tests were enforcing incorrect Haskell code generation for `genericView`
    - Update tests to use `tableNameToViewName` like controller tests.
    - Add tests view names with underscores and camel case.

--- ihp-ide/IHP/IDE/CodeGen/ViewGenerator.hs
+++ ihp-ide/IHP/IDE/CodeGen/ViewGenerator.hs
@@ -81,7 +81,7 @@ buildPlan' schema config =
 
             genericView = [trimming|
                 ${viewHeader}
-                data ${nameWithSuffix} = {${nameWithSuffix}}
+                data ${nameWithSuffix} = ${nameWithSuffix}
 
                 instance View ${nameWithSuffix} where
                     html ${nameWithSuffix} { .. } = [hsx|
