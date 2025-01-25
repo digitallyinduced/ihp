005cd89b0a10a803c543bd17515f01b8f6ba021e
Marc Scholten <marc@digitallyinduced.com>
2025-01-25 09:17:56 -0800

    fix build issue with warp-systemd

--- NixSupport/haskell-packages/warp-systemd.nix
+++ NixSupport/haskell-packages/warp-systemd.nix
@@ -1,5 +1,5 @@
 { mkDerivation, base, http-types, lib, network, systemd, unix, wai
-, warp
+, warp, fetchFromGitHub
 }:
 mkDerivation {
   pname = "warp-systemd";
@@ -12,4 +12,10 @@ mkDerivation {
   description = "Socket activation and other systemd integration for the Warp web server (WAI)";
   license = lib.licenses.bsd3;
   mainProgram = "warp-systemd-example";
+  src = fetchFromGitHub {
+    owner = "hercules-ci";
+    repo = "warp-systemd";
+    rev = "6824c99cbe8e433e42684e78aed36b895beed323";
+    sha256 = "sha256-euSzPbH7J1R1g5qnXpeuGcrJNA4717FzYhHmKYledfg=";
+  };
 }
