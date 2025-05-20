3b3bb8cd38f7e9ed97f77fd8ad82d32a83b29168
Marc Scholten <marc@digitallyinduced.com>
2025-05-20 12:09:24 -0700

    replaced mkGhcCompiler.nix completely with manual overlay

--- NixSupport/overlay.nix
+++ NixSupport/overlay.nix
@@ -1,10 +1,21 @@
 { self, inputs }:
-
+let
+    flakeRoot = self;
+in
 final: prev: {
-    ghc = import ./mkGhcCompiler.nix {
-        pkgs = final;
-        ghcCompiler = final.haskell.packages.ghc98;
-        ihp = self;
-        filter = inputs.nix-filter.lib;
+    ghc = final.haskell.packages.ghc98.override {
+        overrides = self: super: {
+            ihp = super.callPackage "${toString flakeRoot}/ihp.nix" { filter = inputs.nix-filter.lib; };
+            ihp-ide = super.callPackage "${toString flakeRoot}/NixSupport/haskell-packages/ihp-ide.nix" {};
+            ihp-migrate = super.callPackage "${toString flakeRoot}/NixSupport/haskell-packages/ihp-migrate.nix" {};
+            ihp-openai = super.callPackage "${toString flakeRoot}/NixSupport/haskell-packages/ihp-openai.nix" {};
+            ihp-postgresql-simple-extra = super.callPackage "${toString flakeRoot}/NixSupport/haskell-packages/ihp-postgresql-simple-extra.nix" {};
+            ihp-ssc = super.callPackage "${toString flakeRoot}/NixSupport/haskell-packages/ihp-ssc.nix" {};
+            ihp-zip = super.callPackage "${toString flakeRoot}/NixSupport/haskell-packages/ihp-zip.nix" {};
+            ihp-hsx = super.callPackage "${toString flakeRoot}/NixSupport/haskell-packages/ihp-hsx.nix" {};
+            ihp-graphql = super.callPackage "${toString flakeRoot}/NixSupport/haskell-packages/ihp-graphql.nix" {};
+
+            fsnotify = super.callHackageDirect { pkg = "fsnotify"; ver = "0.4.3.0"; sha256 = "sha256-6SJ8w2p0HNhMPjdQzxx4oqsyI48/C/K7wh+kLNy9/fM="; } {};
+        };
     };
 }
