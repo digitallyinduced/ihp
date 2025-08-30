39f1fc89798f9aef65a07152dc79e79ef44bda22
Marc Scholten <marc@digitallyinduced.com>
2025-08-30 20:36:24 +0200

    Rename ProjectGenerator => ihp-new

--- devenv-module.nix
+++ devenv-module.nix
@@ -184,7 +184,7 @@ that is defined in flake-module.nix
             ssc = pkgs.ghc.ihp-ssc;
             migrate = pkgs.ghc.ihp-migrate;
             datasync-typescript = pkgs.ghc.ihp-datasync-typescript;
-            ihp-new = pkgs.callPackage ./ProjectGenerator/default.nix {};
+            ihp-new = pkgs.callPackage ./ihp-new/default.nix {};
 
             guide =
                 let
