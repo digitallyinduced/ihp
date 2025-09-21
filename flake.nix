b0fe63defadc4442e85fcef478184aba9334be30
Marc Scholten <marc@digitallyinduced.com>
2025-09-21 23:12:19 +0200

    use latest

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
     inputs = {
         # "github:NixOS/nixpkgs/nixos-unstable"
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+        nixpkgs.url = "github:sternenseemann/nixpkgs/stackage-lts-24.11";
+
 
         # pre-defined set of default target systems
         systems.url = "github:nix-systems/default";
