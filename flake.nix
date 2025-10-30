54ffb44836445cddea5aae6d554d5554fb0c7d7c
Marc Scholten <marc@digitallyinduced.com>
2025-10-30 12:01:58 -0700

    devenv: v1.8.2 -> v1.10

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
         flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
         # used for setting up development environments
-        devenv.url = "github:cachix/devenv/v1.8.2";
+        devenv.url = "github:cachix/devenv/v1.10";
         devenv.inputs.nixpkgs.follows = "nixpkgs";
 
         # TODO use a corresponding release branch
