446ee415609b2c8afa3145d329d3b409b8a6e0f0
Marc Scholten <marc@digitallyinduced.com>
2026-04-05 18:54:10 +0200

    Fix slow regression fixture imports

--- integration-test/Web/SlowLoad.hs
+++ integration-test/Web/SlowLoad.hs
@@ -2,10 +2,11 @@
 module Web.SlowLoad where
 
 import Control.Concurrent (threadDelay)
-import Language.Haskell.TH.Syntax (runIO)
+import Language.Haskell.TH.Syntax (Dec, runIO)
+import Prelude (Int, pure, (*))
 
 $(do
     -- Keep GHCi in the initial :l Main.hs load long enough for the SIGTERM
     -- regression check to hit the orphaning window deterministically.
-    runIO (threadDelay (10 * 1000000))
-    pure [])
+    runIO (threadDelay (10 * (1000000 :: Int)))
+    pure ([] :: [Dec]))
